fde6f75e4338b0781060986a7269af376ccf0e2f
David Giger <david@akenza.com>
2020-12-11 18:17:36 +0100

    Update downlink.js

--- types/adeunis/ftd-2/downlink.js
+++ types/adeunis/ftd-2/downlink.js
@@ -1,14 +1,16 @@
-function asciiToHexString(input) {
-  var output = "";
-
-  for (var i = 0; i < input.length; i++) {
-    output += input.charCodeAt(i).toString(16);
-  }
-
+function asciiToHexString(input){
+  var output = '';
+  
+  for ( var i = 0; i < input.length; i ++)
+  	output += input.charCodeAt(i).toString(16);
+  
   return output;
 }
 
 function consume(event) {
-  var payload = asciiToHexString(event.payload);
-  emit("downlink", { payload_hex: payload, port: 1 });
+	var payload = asciiToHexString(event.payload.message);
+	var port = event.port ? event.port : 1;
+	var confirmed = event.confirmed ? event.confirmed : false;
+	
+  	emit("downlink", { payload_hex: payload, port: port, confirmed: confirmed });
 }
