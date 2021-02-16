a16816dae995aac02321180a5ee68c3798ff4913
j2 <jonasg@akenza.com>
2021-02-16 16:23:42 +0100

    New Comtac  Devicetype

---
+++ types/comtac/LPN_DO-2/uplink.js
@@ -0,0 +1,21 @@
+var Bits = require('bits');
+
+function consume(event) {
+  var payload = event.data.payload_hex;
+  var bits = Bits.hexToBits(payload);
+  var data = {};
+
+  data.deviceType = Bits.bitsToUnsigned(bits.substr(0, 8));
+  data.version = Bits.bitsToUnsigned(bits.substr(8, 16));
+  data.rssi = Bits.bitsToUnsigned(bits.substr(24, 8)) * -1;
+  data.snr = Bits.bitsToUnsigned(bits.substr(32, 8));
+  data.manually = Bits.bitsToUnsigned(bits.substr(41, 1));
+  data.do_2_lora = Bits.bitsToUnsigned(bits.substr(42, 1));
+  data.do_1_lora = Bits.bitsToUnsigned(bits.substr(43, 1));
+  data.do_2_error = Bits.bitsToUnsigned(bits.substr(44, 1));
+  data.do_1_error = Bits.bitsToUnsigned(bits.substr(45, 1));
+  data.do_2 = Bits.bitsToUnsigned(bits.substr(46, 1));
+  data.do_1 = Bits.bitsToUnsigned(bits.substr(47, 1));
+
+  emit('sample', { data: data, topic: "default" });
+}
