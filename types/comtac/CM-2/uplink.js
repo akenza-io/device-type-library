d6ba7d5f7d22eac89ccb5d6f7d1c640a7c617a25
j2 <jonasg@akenza.com>
2021-03-10 18:46:01 +0100

    tbc

---
+++ types/comtac/CM-2/uplink.js
@@ -0,0 +1,56 @@
+var Bits = require('bits');
+
+function consume(event) {
+  var payload = event.data.payload_hex;
+  var bits = Bits.hexToBits(payload);
+  var data = {};
+  var lifecycle = {};
+
+  lifecycle.version = Bits.bitsToUnsigned(bits.substr(0, 8));
+
+  // Status
+  lifecycle.booster = !!(Bits.bitsToUnsigned(bits.substr(8, 1)));
+  // reserved
+  lifecycle.txOnTimer = !!(Bits.bitsToUnsigned(bits.substr(10, 1)));
+  lifecycle.txOnEvent = !!(Bits.bitsToUnsigned(bits.substr(11, 1)));
+  lifecycle.buttonEvent = !!(Bits.bitsToUnsigned(bits.substr(12, 1)));
+  lifecycle.digitalInputEvent = !!(Bits.bitsToUnsigned(bits.substr(13, 1)));
+  lifecycle.deepSleepEvent = !!(Bits.bitsToUnsigned(bits.substr(14, 1)));
+  lifecycle.digitalInputState = !!(Bits.bitsToUnsigned(bits.substr(15, 1)));
+
+  // Event
+  lifecycle.minTempOn = !!(Bits.bitsToUnsigned(bits.substr(16, 1)));
+  lifecycle.maxTempOn = !!(Bits.bitsToUnsigned(bits.substr(17, 1)));
+  lifecycle.minHumOn = !!(Bits.bitsToUnsigned(bits.substr(18, 1)));
+  lifecycle.maxHumOn = !!(Bits.bitsToUnsigned(bits.substr(19, 1)));
+  lifecycle.minPt100On = !!(Bits.bitsToUnsigned(bits.substr(20, 1)));
+  lifecycle.maxPt100On = !!(Bits.bitsToUnsigned(bits.substr(21, 1)));
+  lifecycle.minLemOn = !!(Bits.bitsToUnsigned(bits.substr(22, 1)));
+  lifecycle.maxLemOn = !!(Bits.bitsToUnsigned(bits.substr(23, 1)));
+  lifecycle.voltage = Bits.bitsToUnsigned(bits.substr(24, 16)) / 1000;
+  emit('sample', { data: lifecycle, topic: "lifecycle" });
+
+  // Data
+  data.temperature = Bits.bitsToSigned(bits.substr(40, 16)) / 100;
+  data.humidity = Bits.bitsToSigned(bits.substr(56, 16)) / 100;
+  data.temperaturePT100 = Bits.bitsToSigned(bits.substr(72, 16)) / 100;
+  data.adc1 = Bits.bitsToUnsigned(bits.substr(88, 16));
+  data.adc2 = Bits.bitsToUnsigned(bits.substr(104, 16));
+  data.lem = Bits.bitsToUnsigned(bits.substr(120, 8));
+  data.brightness = Bits.bitsToUnsigned(bits.substr(128, 8));
+  emit('sample', { data: data, topic: "default" });
+
+
+  if (lifecycle.deepSleepEvent == true) {
+    emit('sample', { data: {}, topic: "sleep" });
+  }
+  if (lifecycle.buttonEvent == true) {
+    emit('sample', { data: {}, topic: "button_pressed" });
+  }
+  if (lifecycle.txOnEvent == true) {
+    emit('sample', { data: {}, topic: "event" });
+  }
+  if (lifecycle.txOnTimer == true) {
+    emit('sample', { data: {}, topic: "timer" });
+  }
+}
