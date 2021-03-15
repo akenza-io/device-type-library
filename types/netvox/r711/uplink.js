01079ac8ce9f13e1ac93e388a8b632205e12153e
j2 <jonasg@akenza.com>
2021-03-15 14:33:53 +0100

    added r711

---
+++ types/netvox/r711/uplink.js
@@ -0,0 +1,17 @@
+var Bits = require('bits')
+
+function consume(event) {
+  var payload = event.data.payload_hex;
+  var bits = Bits.hexToBits(payload);
+  var data = {};
+
+  //Header
+  data.version = Bits.bitsToUnsigned(bits.substr(0, 8));
+  data.deviceType = Bits.bitsToUnsigned(bits.substr(8, 8));
+  data.battery = Bits.bitsToUnsigned(bits.substr(24, 8)) / 10;
+
+  emit('sample', { data: { "temperature": Bits.bitsToSigned(bits.substr(32, 16)) / 100, "humidity": Bits.bitsToUnsigned(bits.substr(48, 16)) / 100 }, topic: "default" });
+
+  // Last 3 Bytes are reserved
+  emit('sample', { data: data, topic: "lifecycle" });
+}
