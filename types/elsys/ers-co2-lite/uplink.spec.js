021abc23bdfcf246276876461fa2bb295ab381d7
j2 <jonasg@akenza.com>
2021-03-19 15:54:37 +0100

    ers co2 lite & ers-lite

---
+++ types/elsys/ers-co2-lite/uplink.spec.js
@@ -0,0 +1,73 @@
+const chai = require("chai");
+const validate = require("jsonschema").validate;
+const rewire = require("rewire");
+const fs = require("fs");
+
+const assert = chai.assert;
+
+const script = rewire("./uplink.js");
+let defaultSchema = null;
+let lifecycleSchema = null;
+const consume = script.__get__("consume");
+
+function expectEmit(callback) {
+  script.__set__({
+    emit: callback,
+  });
+}
+
+before(function (done) {
+  fs.readFile(
+    __dirname + "/default.schema.json",
+    "utf8",
+    function (err, fileContents) {
+      if (err) throw err;
+      defaultSchema = JSON.parse(fileContents);
+      done();
+    }
+  );
+});
+
+before(function (done) {
+  fs.readFile(
+    __dirname + "/lifecycle.schema.json",
+    "utf8",
+    function (err, fileContents) {
+      if (err) throw err;
+      lifecycleSchema = JSON.parse(fileContents);
+      done();
+    }
+  );
+});
+
+describe("Elsys CO2 lite uplink", function () {
+  describe("consume()", function () {
+    it("should decode Elsys CO2 lite payload", function (done) {
+      const data = {
+        data: {
+          payload_hex: "0100f4022804000b05000601d8070e3e",
+        },
+      };
+
+      expectEmit(function (type, value) {
+        assert.equal(type, "sample");
+        assert.isNotNull(value);
+        assert.typeOf(value.data, "object");
+
+        if (value.topic == "lifecycle") {
+          assert.equal(value.data.voltage, 3.646);
+          validate(value.data, lifecycleSchema, { throwError: true });
+        }
+
+        if (value.topic == "default") {
+          assert.equal(value.data.humidity, 40);
+          assert.equal(value.data.temperature, 24.4);
+          validate(value.data, defaultSchema, { throwError: true });
+        }
+      });
+
+      consume(data);
+      done();
+    });
+  });
+});
