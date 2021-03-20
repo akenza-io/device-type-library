24996102ff5c70bf5e35135a1f863c7b03ff5970
j2 <jonasg@akenza.com>
2021-03-20 15:17:41 +0100

    added rak612

---
+++ types/rak/RAK612/uplink.js
@@ -0,0 +1,19 @@
+var Bits = require('bits')
+
+function consume(event) {
+  var payload = event.data.payload_hex;
+  var bits = Bits.hexToBits(payload);
+  var data = {};
+  var lifecycle = {};
+
+  lifecycle.dataType = Bits.bitsToUnsigned(bits.substr(8, 8));
+  data.key1 = !!Bits.bitsToUnsigned(bits.substr(16, 8));
+  data.key2 = !!Bits.bitsToUnsigned(bits.substr(24, 8));
+  data.key3 = !!Bits.bitsToSigned(bits.substr(32, 8));
+  data.key4 = !!Bits.bitsToSigned(bits.substr(40, 8));
+  lifecycle.voltage = Bits.bitsToSigned(bits.substr(48, 8));
+  lifecycle.statusPercent = Bits.bitsToSigned(bits.substr(56, 8));
+
+  emit('sample', { "data": data, "topic": "default" });
+  emit('sample', { "data": data, "lifecycle": "lifecycle" });
+}
