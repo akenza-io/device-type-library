9463f1eea6b954d11040bdfe90f355dee6cf3954
j2 <jonasg@akenza.com>
2021-03-22 11:50:13 +0100

    fix

--- types/oxon/buttonboard/uplink.spec.js
+++ types/oxon/buttonboard/uplink.spec.js
@@ -2,21 +2,16 @@ const chai = require("chai");
 const validate = require("jsonschema").validate;
 const rewire = require("rewire");
 const fs = require("fs");
-
 const assert = chai.assert;
-
 const script = rewire("./uplink.js");
 let defaultSchema = null;
 let lifecycleSchema = null;
 const consume = script.__get__("consume");
-
 function expectEmit(callback) {
   script.__set__({
     emit: callback,
   });
 }
-
-
 before(function (done) {
   fs.readFile(
     __dirname + "/default.schema.json",
@@ -39,7 +34,6 @@ before(function (done) {
     }
   );
 });
-
 describe("Oxon Touchpanel Uplink", function () {
   describe("consume()", function () {
     it("should decode the Oxon Touchpanel payload", function () {
@@ -50,20 +44,16 @@ describe("Oxon Touchpanel Uplink", function () {
         },
       };
       let sampleCount = 0;
-
       expectEmit(function (type, value) {
         assert.equal(type, "sample");
         assert.isNotNull(value);
         assert.typeOf(value.data, "object");
-
         if (value.topic === "lifecycle") {
           sampleCount++;
           assert.equal(value.data.statusPercent, 100);
           assert.equal(value.data.heartbeat, true);
-
           validate(value.data, lifecycleSchema, { throwError: true });
         }
-
         if (value.topic === "default") {
           sampleCount++;
           assert.equal(value.data.msgType, 49);
@@ -71,7 +61,6 @@ describe("Oxon Touchpanel Uplink", function () {
           assert.equal(value.data.imageH, 1);
           assert.equal(value.data.imageID, 63);
           assert.equal(value.data.temperature, 18);
-
           validate(value.data, defaultSchema, { throwError: true });
         }
       });
