ff9ff14dab0bf489b052c861f0e7c92fdda24bd9
Marius Giger <marius@akenza.io>
2021-05-09 21:34:52 +0200

    adds ESLint

--- types/adeunis/ftdNetwokTester/uplink.js
+++ types/adeunis/ftdNetwokTester/uplink.js
@@ -1,24 +1,25 @@
 function parseHexString(str) {
-  var result = [];
-  while (str.length >= 2) {
-    result.push(parseInt(str.substring(0, 2), 16));
-    str = str.substring(2, str.length);
+  let tempStr = str;
+  const result = [];
+  while (tempStr.length >= 2) {
+    result.push(parseInt(tempStr.substring(0, 2), 16));
+    tempStr = tempStr.substring(2, tempStr.length);
   }
 
   return result;
 }
 
 function consume(event) {
-  var payload = event.data.payload_hex;
-  var bytes = parseHexString(payload);
-  var decoded = {};
-  var offset = 0;
+  const payload = event.data.payload_hex;
+  const bytes = parseHexString(payload);
+  const decoded = {};
+  let offset = 0;
 
   if (offset + 1 <= bytes.length) {
-    var status = bytes[offset++];
+    const status = bytes[(offset += 1)];
     if (status & 0x80 && offset + 1 < bytes.length) {
-      var temperature = bytes[offset];
-      if (temperature > 127) temperature = temperature - 256;
+      let temperature = bytes[offset];
+      if (temperature > 127) temperature -= 256;
 
       decoded.temperature = temperature;
       offset += 1;
@@ -33,14 +34,14 @@ function consume(event) {
     }
 
     if (status & 0x10 && offset + 9 <= bytes.length) {
-      var latDeg10 = bytes[offset] >> 4;
-      var latDeg1 = bytes[offset] & 0x0f;
-      var latMin10 = bytes[offset + 1] >> 4;
-      var latMin1 = bytes[offset + 1] & 0x0f;
-      var latMin01 = bytes[offset + 2] >> 4;
-      var latMin001 = bytes[offset + 2] & 0x0f;
-      var latMin0001 = bytes[offset + 3] >> 4;
-      var latSign = bytes[offset + 3] & 0x01 ? -1 : 1;
+      const latDeg10 = bytes[offset] >> 4;
+      const latDeg1 = bytes[offset] & 0x0f;
+      const latMin10 = bytes[offset + 1] >> 4;
+      const latMin1 = bytes[offset + 1] & 0x0f;
+      const latMin01 = bytes[offset + 2] >> 4;
+      const latMin001 = bytes[offset + 2] & 0x0f;
+      const latMin0001 = bytes[offset + 3] >> 4;
+      const latSign = bytes[offset + 3] & 0x01 ? -1 : 1;
       decoded.latitude =
         latSign *
         (latDeg10 * 10 +
@@ -51,14 +52,14 @@ function consume(event) {
             latMin001 * 0.01 +
             latMin0001 * 0.001) /
             60);
-      var lonDeg100 = bytes[offset + 4] >> 4;
-      var lonDeg10 = bytes[offset + 4] & 0x0f;
-      var lonDeg1 = bytes[offset + 5] >> 4;
-      var lonMin10 = bytes[offset + 5] & 0x0f;
-      var lonMin1 = bytes[offset + 6] >> 4;
-      var lonMin01 = bytes[offset + 6] & 0x0f;
-      var lonMin001 = bytes[offset + 7] >> 4;
-      var lonSign = bytes[offset + 7] & 0x01 ? -1 : 1;
+      const lonDeg100 = bytes[offset + 4] >> 4;
+      const lonDeg10 = bytes[offset + 4] & 0x0f;
+      const lonDeg1 = bytes[offset + 5] >> 4;
+      const lonMin10 = bytes[offset + 5] & 0x0f;
+      const lonMin1 = bytes[offset + 6] >> 4;
+      const lonMin01 = bytes[offset + 6] & 0x0f;
+      const lonMin001 = bytes[offset + 7] >> 4;
+      const lonSign = bytes[offset + 7] & 0x01 ? -1 : 1;
       decoded.longitude =
         lonSign *
         (lonDeg100 * 100 +
@@ -87,8 +88,8 @@ function consume(event) {
 
     if (status & 0x01 && offset + 2 <= bytes.length) {
       decoded.rssi = -bytes[offset];
-      var snr = bytes[offset + 1];
-      if (snr > 127) snr = snr - 256;
+      let snr = bytes[offset + 1];
+      if (snr > 127) snr -= 256;
 
       decoded.snr = snr;
       offset += 2;
