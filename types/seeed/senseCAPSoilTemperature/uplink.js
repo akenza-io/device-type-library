2ff19e41aad877132cb98c64e37483de84033a9a
j2 <jonasg@akenza.com>
2021-05-21 14:27:34 +0200

    added senseCAP

---
+++ types/seeed/senseCAPSoilTemperature/uplink.js
@@ -0,0 +1,25 @@
+function toLittleEndianSigned(hex) {
+  // Creating little endian hex DCBA
+  const hexArray = [];
+  let tempHex = hex;
+  while (tempHex.length >= 2) {
+    hexArray.push(tempHex.substring(0, 2));
+    tempHex = tempHex.substring(2, tempHex.length);
+  }
+  hexArray.reverse();
+  // To signed
+  return Bits.bitsToSigned(Bits.hexToBits(hexArray.join("")));
+}
+
+function consume(event) {
+  const payload = event.data.payload_hex;
+  const data = {};
+  // 0-2 Channel Number
+  // 2-4 Measurement ID
+  data.temperature = toLittleEndianSigned(payload.substr(6, 8)) / 1000;
+  // 14-16 Channel Number
+  // 16-20 Measurement ID
+  data.soilMoisture = toLittleEndianSigned(payload.substr(20, 8)) / 1000;
+
+  emit("sample", { data, topic: "default" });
+}
---
+++ types/seeed/senseCAPSoilTemperature/uplink.spec.js
@@ -0,0 +1,66 @@
+const chai = require("chai");
+const { validate } = require("jsonschema");
+const rewire = require("rewire");
+const axios = require("axios");
+const fs = require("fs");
+const Ajv = require("ajv");
+
+const { assert } = chai;
+
+const script = rewire("./uplink.js");
+let defaultSchema = null;
+const consume = script.__get__("consume");
+
+function expectEmit(callback) {
+  script.__set__({
+    emit: callback,
+  });
+}
+
+before((done) => {
+  fs.readFile(
+    `${__dirname}/default.schema.json`,
+    "utf8",
+    (err, fileContents) => {
+      if (err) throw err;
+      defaultSchema = JSON.parse(fileContents);
+      done();
+    },
+  );
+});
+
+function loadRemoteSchema(uri) {
+  return axios.get(uri).then((res) => {
+    if (res.status >= 400) {
+      throw new Error(`Schema loading error: ${res.statusCode}`);
+    }
+    return res.data;
+  });
+}
+
+describe("Seeed SenseCAP Wireless Soil Moisture and Temperature Sensor Uplink", () => {
+  describe("consume()", () => {
+    it("should decode Seeed SenseCAP Wireless Soil Moisture and Temperature Sensor payload", () => {
+      const data = {
+        data: {
+          port: 1,
+          payload_hex: "010610007D0000010710725100009A21",
+        },
+      };
+
+      expectEmit((type, value) => {
+        assert.equal(type, "sample");
+        assert.isNotNull(value);
+        assert.typeOf(value.data, "object");
+
+        if (value.topic === "default") {
+          assert.equal(value.data.temperature, 32);
+          assert.equal(value.data.soilMoisture, 20.85);
+          validate(value.data, defaultSchema, { throwError: true });
+        }
+      });
+
+      consume(data);
+    });
+  });
+});
