014c004e9971601f4b500de4b4f35ed9edcd6017
j2 <jonasg@akenza.com>
2021-06-01 15:39:49 +0200

    add changelog later

--- types/comtac/CM-2/uplink.spec.js
+++ types/comtac/CM-2/uplink.spec.js
@@ -43,26 +43,25 @@ describe("Comtac LPN CM-2 Uplink", () => {
         assert.isNotNull(value);
         assert.typeOf(value.data, "object");
 
-        if (value.topic === "lifecyle") {
-          assert.equal(value.data.booster, false);
-          assert.equal(value.data.txOnTimer, false);
-          assert.equal(value.data.txOnEvent, false);
-          assert.equal(value.data.buttonEvent, true);
-          assert.equal(value.data.digitalInputEvent, false);
-          assert.equal(value.data.deepSleepEvent, false);
-          assert.equal(value.data.digitalInputState, false);
-          assert.equal(value.data.minTempOn, false);
-          assert.equal(value.data.maxTempOn, false);
-          assert.equal(value.data.minHumOn, false);
-          assert.equal(value.data.maxHumOn, false);
-          assert.equal(value.data.minPt100On, false);
-          assert.equal(value.data.maxPt100On, false);
-          assert.equal(value.data.minLemOn, false);
-          assert.equal(value.data.maxLemOn, false);
-          assert.equal(value.data.voltage, 2.654);
+        assert.equal(value.topic, "lifecycle");
+        assert.equal(value.data.booster, false);
+        assert.equal(value.data.txOnTimer, false);
+        assert.equal(value.data.txOnEvent, false);
+        assert.equal(value.data.buttonEvent, true);
+        assert.equal(value.data.digitalInputEvent, false);
+        assert.equal(value.data.deepSleepEvent, false);
+        assert.equal(value.data.digitalInputState, false);
+        assert.equal(value.data.minTempOn, false);
+        assert.equal(value.data.maxTempOn, false);
+        assert.equal(value.data.minHumOn, false);
+        assert.equal(value.data.maxHumOn, false);
+        assert.equal(value.data.minPt100On, false);
+        assert.equal(value.data.maxPt100On, false);
+        assert.equal(value.data.minLemOn, false);
+        assert.equal(value.data.maxLemOn, false);
+        assert.equal(value.data.voltage, 2.654);
 
-          validate(value.data, lifecycleSchema, { throwError: true });
-        }
+        validate(value.data, lifecycleSchema, { throwError: true });
       });
 
       utils.expectEmits((type, value) => {
@@ -70,18 +69,26 @@ describe("Comtac LPN CM-2 Uplink", () => {
         assert.isNotNull(value);
         assert.typeOf(value.data, "object");
 
-        if (value.topic === "default") {
-          assert.equal(value.data.temperature, -3);
-          assert.equal(value.data.humidity, 66.36);
-          assert.equal(value.data.temperaturePT100, 20);
-          assert.equal(value.data.adc1, 500);
-          assert.equal(value.data.adc2, 1096);
-          assert.equal(value.data.lem, 2.506);
-          assert.equal(value.data.brightness, 21);
-          validate(value.data, defaultSchema, { throwError: true });
-        }
+        assert.equal(value.topic, "default");
+        assert.equal(value.data.temperature, -3);
+        assert.equal(value.data.humidity, 66.36);
+        assert.equal(value.data.temperaturePT100, 20);
+        assert.equal(value.data.adc1, 500);
+        assert.equal(value.data.adc2, 1096);
+        assert.equal(value.data.lem, 2.506);
+        assert.equal(value.data.brightness, 21);
+        validate(value.data, defaultSchema, { throwError: true });
       });
+      /*
+      utils.expectEmits((type, value) => {
+        assert.equal(type, "sample");
+        assert.isNotNull(value);
+        assert.typeOf(value.data, "object");
 
+        assert.equal(value.topic, "button_pressed");
+        assert.equal(value.data.buttonPressed, true);
+      });
+*/
       consume(data);
     });
   });
