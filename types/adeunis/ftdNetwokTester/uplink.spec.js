fe768a349d1f61e1904b2fc6324c7eb6409382e9
j2 <jonasg@akenza.com>
2021-10-06 17:39:17 +0200

    tbc

--- types/adeunis/ftdNetwokTester/uplink.spec.js
+++ types/adeunis/ftdNetwokTester/uplink.spec.js
@@ -1,49 +1,68 @@
 const chai = require("chai");
 const { validate } = require("jsonschema");
 const rewire = require("rewire");
-const fs = require("fs");
+const utils = require("test-utils");
 
 const { assert } = chai;
 
-const script = rewire("./uplink.js");
-let schema = null;
-const consume = script.__get__("consume");
-
-describe("Adeunis FTD-2 Uplink", () => {
-  function expectEmit(callback) {
-    script.__set__({
-      emit: callback,
-    });
-  }
+describe("FTD Network tester", () => {
+  let defaultSchema = null;
+  let consume = null;
   before((done) => {
-    fs.readFile(
-      `${__dirname}/default.schema.json`,
-      "utf8",
-      (err, fileContents) => {
-        if (err) throw err;
-        schema = JSON.parse(fileContents);
+    const script = rewire("./uplink.js");
+    consume = utils.init(script);
+    utils
+      .loadSchema(`${__dirname}/default.schema.json`)
+      .then((parsedSchema) => {
+        defaultSchema = parsedSchema;
         done();
-      },
-    );
+      });
   });
+
+  let gpsSchema = null;
+  before((done) => {
+    utils.loadSchema(`${__dirname}/gps.schema.json`).then((parsedSchema) => {
+      gpsSchema = parsedSchema;
+      done();
+    });
+  });
+
   describe("consume()", () => {
-    it("should decode Adeunis payload", () => {
+    it("should decode the FTD Network payload", () => {
       const data = {
         data: {
-          payloadHex: "1234abcdb000000b",
+          port: 3,
+          payloadHex: "be1a47248480008320601512100da1",
         },
       };
 
-      expectEmit((type, value) => {
+      utils.expectEmits((type, value) => {
         assert.equal(type, "sample");
         assert.isNotNull(value);
         assert.typeOf(value.data, "object");
+
+        assert.equal(value.topic, "lifecycle");
+        assert.equal(value.data.batteryVoltage, 3.66);
+        assert.equal(value.data.batteryLevel, 0);
+        assert.equal(value.data.deviceStatusFlag, 0);
+
+        validate(value.data, gpsSchema, { throwError: true });
+      });
+
+      utils.expectEmits((type, value) => {
+        assert.equal(type, "sample");
+        assert.isNotNull(value);
+        assert.typeOf(value.data, "object");
+
         assert.equal(value.topic, "default");
-
-        assert.equal(value.data.trigger, "PUSHBUTTON");
-        assert.equal(value.data.downlink, 52);
-
-        validate(value.data, schema, { throwError: true });
+        assert.equal(value.data.currentLevel, 2.9);
+        assert.equal(value.data.isEmptying, false);
+        assert.equal(value.data.isTanking, false);
+        assert.equal(value.data.hasMeasurementError, false);
+        assert.equal(value.data.hasOutOfRangeError, true);
+        assert.equal(value.data.sequenceNumber, 2);
+        assert.equal(value.data.temperature, 23);
+        validate(value.data, defaultSchema, { throwError: true });
       });
 
       consume(data);
