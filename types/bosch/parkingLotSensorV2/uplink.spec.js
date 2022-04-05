7acfde097f2dcbf3bc535cc5452c0e2a174a1774
j2 <jonasg@akenza.com>
2022-04-05 10:27:33 +0200

    tbc

---
+++ types/bosch/parkingLotSensorV2/uplink.spec.js
@@ -0,0 +1,64 @@
+const chai = require("chai");
+const { validate } = require("jsonschema");
+const rewire = require("rewire");
+const utils = require("test-utils");
+
+const { assert } = chai;
+
+describe("Bosch Parking Lot Sensor Uplink", () => {
+  let occupancySchema = null;
+  let consume = null;
+  before((done) => {
+    const script = rewire("./uplink.js");
+    consume = utils.init(script);
+    utils
+      .loadSchema(`${__dirname}/occupancy.schema.json`)
+      .then((parsedSchema) => {
+        occupancySchema = parsedSchema;
+        done();
+      });
+  });
+
+  describe("consume()", () => {
+    it("should decode the Bosch Parking Lot Sensor payload", () => {
+      const data = {
+        data: {
+          port: 2,
+          payloadHex: "00",
+        },
+      };
+
+      utils.expectEmits((type, value) => {
+        assert.equal(type, "sample");
+        assert.isNotNull(value);
+        assert.typeOf(value.data, "object");
+        assert.equal(value.topic, "occupancy");
+        assert.equal(value.data.occupancy, 0);
+
+        validate(value.data, occupancySchema, { throwError: true });
+      });
+
+      consume(data);
+    });
+
+    it("should decode the Bosch Parking Lot Sensor payload", () => {
+      const data = {
+        data: {
+          port: 2,
+          payloadHex: "01",
+        },
+      };
+
+      utils.expectEmits((type, value) => {
+        assert.equal(type, "sample");
+        assert.isNotNull(value);
+        assert.typeOf(value.data, "object");
+        assert.equal(value.topic, "occupancy");
+        assert.equal(value.data.occupancy, 1);
+        validate(value.data, occupancySchema, { throwError: true });
+      });
+
+      consume(data);
+    });
+  });
+});
