695e5e6cff2ae064bde63010d8ac5d15874da8f8
j2 <jonasg@akenza.com>
2022-05-17 14:09:10 +0200

    Added talkpool oy1110

---
+++ types/talkpool/oy1110/uplink.js
@@ -0,0 +1,41 @@
+function consume(event) {
+  const payload = event.data.payloadHex;
+  const { port } = event.data;
+  const data = {};
+
+  let dataBytes = Buffer.from(payload, "hex");
+  // console.log(dataBytes.toString('base64'));
+
+  if (port === 1) {
+    // Status
+  } else if (port === 2 || port === 3) {
+    if (dataBytes.length % 3 !== 0) {
+      // return null;
+    }
+    if (port === 3) {
+      dataBytes = dataBytes.slice(1, dataBytes.length);
+    }
+
+    let index = 0;
+    let time;
+    const capacity = dataBytes.length / 3;
+    const d = new Date();
+
+    for (index = 0; index < capacity; index++) {
+      d.setMinutes(d.getMinutes() - 15 * index);
+
+      data.temperature =
+        (((dataBytes[index * 3] << 4) |
+          ((dataBytes[index * 3 + 2] & 0xf0) >> 4)) -
+          800) /
+        10.0;
+      data.humidity =
+        (((dataBytes[index * 3 + 1] << 4) | (dataBytes[index * 3 + 2] & 0x0f)) -
+          250) /
+        10.0;
+
+      time = d.toISOString();
+      emit("sample", { data, topic: "default", timestamp: time });
+    }
+  }
+}
---
+++ types/talkpool/oy1110/uplink.spec.js
@@ -0,0 +1,70 @@
+const chai = require("chai");
+const { validate } = require("jsonschema");
+const rewire = require("rewire");
+const utils = require("test-utils");
+
+const { assert } = chai;
+
+describe("Talkpool OY1110 Uplink", () => {
+  let defaultSchema = null;
+  let consume = null;
+  before((done) => {
+    const script = rewire("./uplink.js");
+    consume = utils.init(script);
+    utils
+      .loadSchema(`${__dirname}/default.schema.json`)
+      .then((parsedSchema) => {
+        defaultSchema = parsedSchema;
+        done();
+      });
+  });
+
+  describe("consume()", () => {
+    it("should decode the Talkpool OY1110 report uplink", () => {
+      const data = {
+        data: {
+          port: 3,
+          payloadHex: "0f2e3ccd3338d23931f5",
+        },
+      };
+
+      utils.expectEmits((type, value) => {
+        assert.equal(type, "sample");
+        assert.isNotNull(value);
+        assert.typeOf(value.data, "object");
+
+        assert.equal(value.topic, "default");
+        assert.equal(value.data.temperature, -5.2);
+        assert.equal(value.data.humidity, 72.3);
+
+        validate(value.data, defaultSchema, { throwError: true });
+      });
+
+      utils.expectEmits((type, value) => {
+        assert.equal(type, "sample");
+        assert.isNotNull(value);
+        assert.typeOf(value.data, "object");
+
+        assert.equal(value.topic, "default");
+        assert.equal(value.data.temperature, 2.9);
+        assert.equal(value.data.humidity, 64.8);
+
+        validate(value.data, defaultSchema, { throwError: true });
+      });
+
+      utils.expectEmits((type, value) => {
+        assert.equal(type, "sample");
+        assert.isNotNull(value);
+        assert.typeOf(value.data, "object");
+
+        assert.equal(value.topic, "default");
+        assert.equal(value.data.temperature, 12.7);
+        assert.equal(value.data.humidity, 53.9);
+
+        validate(value.data, defaultSchema, { throwError: true });
+      });
+
+      consume(data);
+    });
+  });
+});
