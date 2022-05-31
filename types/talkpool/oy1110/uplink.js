052c198a878b560bf1ca6be6ca91855a2876926a
David Giger <david@akenza.io>
2022-05-31 18:01:09 +0200

    fix: talkpool oy1110 device type by using hexToBytes instead of Buffer.from(...)

--- types/talkpool/oy1110/uplink.js
+++ types/talkpool/oy1110/uplink.js
@@ -2,7 +2,7 @@ function consume(event) {
   const payload = event.data.payloadHex;
   const { port } = event.data;
   const data = {};
-  let dataBytes = Buffer.from(payload, "hex");
+  let dataBytes = hexToBytes(payload);
 
   if (port === 1) {
     if (payload === "012000") {
@@ -17,12 +17,11 @@ function consume(event) {
       dataBytes = dataBytes.slice(1, dataBytes.length);
     }
 
-    let index = 0;
     let time;
     const capacity = dataBytes.length / 3;
     const d = new Date();
 
-    for (index = 0; index < capacity; index++) {
+    for (let index = 0; index < capacity; index++) {
       d.setMinutes(d.getMinutes() - 15 * index);
 
       data.temperature =
@@ -39,4 +38,15 @@ function consume(event) {
       emit("sample", { data, topic: "default", timestamp: time });
     }
   }
+}
+
+// Note: Hex.hexToBytes(...) can't be used here as the returned list does not support slicing...
+function hexToBytes(hex) {
+  const bytes = [];
+  let c = 0;
+  while (c < hex.length) {
+    bytes.push(parseInt(hex.substr(c, 2), 16));
+    c += 2;
+  }
+  return bytes;
 }
