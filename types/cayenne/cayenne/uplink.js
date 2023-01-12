a0182581c2cf3ccccce8f1d701cb10597cd37eeb
j2 <jonasg@akenza.com>
2023-01-12 10:48:26 +0100

    tbc

---
+++ types/cayenne/cayenne/uplink.js
@@ -0,0 +1,94 @@
+function consume(event) {
+  const payload = event.data.payloadHex;
+  const bits = Bits.hexToBits(payload);
+
+  for (let pointer = 0; pointer !== payload.length; ) {
+    let topic = "default";
+    const data = {};
+    data.channel = Bits.bitsToUnsigned(bits.substr(pointer, 8));
+    pointer += 8;
+    const flag = Bits.bitsToUnsigned(bits.substr(pointer, 8));
+    pointer += 8;
+
+    switch (flag) {
+      case 0x00:
+        topic = "digital";
+        data.digitalInput = Bits.bitsToUnsigned(bits.substr(pointer, 8));
+        pointer += 8;
+        break;
+      case 0x01:
+        topic = "digital";
+        data.digitalOutput = Bits.bitsToUnsigned(bits.substr(pointer, 8));
+        pointer += 8;
+        break;
+      case 0x02:
+        topic = "analog";
+        data.analogInput = Bits.bitsToUnsigned(bits.substr(pointer, 16)) / 100;
+        pointer += 16;
+        break;
+      case 0x03:
+        topic = "analog";
+        data.analogOutput = Bits.bitsToUnsigned(bits.substr(pointer, 16)) / 100;
+        pointer += 16;
+        break;
+      case 0x65:
+        topic = "light";
+        data.light = Bits.bitsToUnsigned(bits.substr(pointer, 16));
+        pointer += 16;
+        break;
+      case 0x66:
+        topic = "presence";
+        data.presence = Bits.bitsToUnsigned(bits.substr(pointer, 8));
+        pointer += 8;
+        break;
+      case 0x67:
+        topic = "temperature";
+        data.temperature = Bits.bitsToSigned(bits.substr(pointer, 16)) / 10;
+        pointer += 16;
+        break;
+      case 0x68:
+        topic = "humidity";
+        data.humidity = Bits.bitsToUnsigned(bits.substr(pointer, 8));
+        pointer += 8;
+        break;
+      case 0x71:
+        topic = "accelerometer";
+        data.xAxisAccelerometer = Bits.bitsToSigned(bits.substr(pointer, 16));
+        pointer += 16;
+        data.yAxisAccelerometer = Bits.bitsToSigned(bits.substr(pointer, 16));
+        pointer += 16;
+        data.zAxisAccelerometer = Bits.bitsToSigned(bits.substr(pointer, 16));
+        pointer += 16;
+        break;
+      case 0x73:
+        topic = "barometer";
+        data.barometer = Bits.bitsToSigned(bits.substr(pointer, 16));
+        pointer += 16;
+        break;
+      case 0x86:
+        topic = "gyrometer";
+        data.gyroX = Bits.bitsToSigned(bits.substr(pointer, 16));
+        pointer += 16;
+        data.gyroY = Bits.bitsToSigned(bits.substr(pointer, 16));
+        pointer += 16;
+        data.gyroZ = Bits.bitsToSigned(bits.substr(pointer, 16));
+        pointer += 16;
+        break;
+      case 0x88:
+        topic = "gps";
+        data.latitude = Bits.bitsToSigned(bits.substr(pointer, 24)) / 10000;
+        pointer += 24;
+        data.longitude = Bits.bitsToSigned(bits.substr(pointer, 24)) / 10000;
+        pointer += 24;
+        data.altitude = Bits.bitsToSigned(bits.substr(pointer, 24)) / 100;
+        pointer += 24;
+        break;
+      default:
+        pointer = payload.length;
+        break;
+    }
+    if (Object.keys(data).length > 1) {
+      emit("sample", { data, topic });
+    }
+  }
+}
---
+++ types/cayenne/cayenne/uplink.spec.js
@@ -0,0 +1,200 @@
+const chai = require("chai");
+
+const rewire = require("rewire");
+const utils = require("test-utils");
+
+const { assert } = chai;
+
+describe("Cayenne uplink", () => {
+  let accelerometerSchema = null;
+  let consume = null;
+  before((done) => {
+    const script = rewire("./uplink.js");
+    consume = utils.init(script);
+    utils
+      .loadSchema(`${__dirname}/accelerometer.schema.json`)
+      .then((parsedSchema) => {
+        accelerometerSchema = parsedSchema;
+        done();
+      });
+  });
+
+  let analogSchema = null;
+  before((done) => {
+    utils.loadSchema(`${__dirname}/analog.schema.json`).then((parsedSchema) => {
+      analogSchema = parsedSchema;
+      done();
+    });
+  });
+
+  let barometerSchema = null;
+  before((done) => {
+    utils
+      .loadSchema(`${__dirname}/barometer.schema.json`)
+      .then((parsedSchema) => {
+        barometerSchema = parsedSchema;
+        done();
+      });
+  });
+
+  let digitalSchema = null;
+  before((done) => {
+    utils
+      .loadSchema(`${__dirname}/digital.schema.json`)
+      .then((parsedSchema) => {
+        digitalSchema = parsedSchema;
+        done();
+      });
+  });
+
+  let gpsSchema = null;
+  before((done) => {
+    utils.loadSchema(`${__dirname}/gps.schema.json`).then((parsedSchema) => {
+      gpsSchema = parsedSchema;
+      done();
+    });
+  });
+
+  let gyrometerSchema = null;
+  before((done) => {
+    utils
+      .loadSchema(`${__dirname}/gyrometer.schema.json`)
+      .then((parsedSchema) => {
+        gyrometerSchema = parsedSchema;
+        done();
+      });
+  });
+
+  let humiditySchema = null;
+  before((done) => {
+    utils
+      .loadSchema(`${__dirname}/humidity.schema.json`)
+      .then((parsedSchema) => {
+        humiditySchema = parsedSchema;
+        done();
+      });
+  });
+
+  let lightSchema = null;
+  before((done) => {
+    utils.loadSchema(`${__dirname}/light.schema.json`).then((parsedSchema) => {
+      lightSchema = parsedSchema;
+      done();
+    });
+  });
+
+  let temperatureSchema = null;
+  before((done) => {
+    utils
+      .loadSchema(`${__dirname}/temperature.schema.json`)
+      .then((parsedSchema) => {
+        temperatureSchema = parsedSchema;
+        done();
+      });
+  });
+
+  let presenceSchema = null;
+  before((done) => {
+    utils
+      .loadSchema(`${__dirname}/presence.schema.json`)
+      .then((parsedSchema) => {
+        presenceSchema = parsedSchema;
+        done();
+      });
+  });
+
+  describe("consume()", () => {
+    it("should decode the Cayenne Temperature payload", () => {
+      const data = {
+        data: {
+          port: 1,
+          payloadHex: "03670110056700FF",
+        },
+      };
+
+      utils.expectEmits((type, value) => {
+        assert.equal(type, "sample");
+        assert.isNotNull(value);
+        assert.typeOf(value.data, "object");
+
+        assert.equal(value.topic, "temperature");
+        assert.equal(value.data.channel, 3);
+        assert.equal(value.data.temperature, 27.2);
+
+        utils.validateSchema(value.data, temperatureSchema, {
+          throwError: true,
+        });
+      });
+
+      utils.expectEmits((type, value) => {
+        assert.equal(type, "sample");
+        assert.isNotNull(value);
+        assert.typeOf(value.data, "object");
+
+        assert.equal(value.topic, "temperature");
+        assert.equal(value.data.channel, 5);
+        assert.equal(value.data.temperature, 25.5);
+
+        utils.validateSchema(value.data, temperatureSchema, {
+          throwError: true,
+        });
+      });
+
+      consume(data);
+    });
+
+    it("should decode the Cayenne Accelerometer payload", () => {
+      const data = {
+        data: {
+          port: 1,
+          payloadHex: "067104D2FB2E0000",
+        },
+      };
+
+      utils.expectEmits((type, value) => {
+        assert.equal(type, "sample");
+        assert.isNotNull(value);
+        assert.typeOf(value.data, "object");
+
+        assert.equal(value.topic, "accelerometer");
+        assert.equal(value.data.channel, 6);
+        assert.equal(value.data.xAxisAccelerometer, 1234);
+        assert.equal(value.data.yAxisAccelerometer, -1234);
+        assert.equal(value.data.zAxisAccelerometer, 0);
+
+        utils.validateSchema(value.data, accelerometerSchema, {
+          throwError: true,
+        });
+      });
+
+      consume(data);
+    });
+
+    it("should decode the Cayenne GPS payload", () => {
+      const data = {
+        data: {
+          port: 1,
+          payloadHex: "018806765ff2960a0003e8",
+        },
+      };
+
+      utils.expectEmits((type, value) => {
+        assert.equal(type, "sample");
+        assert.isNotNull(value);
+        assert.typeOf(value.data, "object");
+
+        assert.equal(value.topic, "gps");
+        assert.equal(value.data.channel, 1);
+        assert.equal(value.data.latitude, 42.3519);
+        assert.equal(value.data.longitude, -87.9094);
+        assert.equal(value.data.altitude, 10);
+
+        utils.validateSchema(value.data, gpsSchema, {
+          throwError: true,
+        });
+      });
+
+      consume(data);
+    });
+  });
+});
