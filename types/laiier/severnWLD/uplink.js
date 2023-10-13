9ceb9dfb386c885a59e1f99cdc8bd91b6070bc03
j2 <jonasg@akenza.com>
2023-10-13 11:38:38 +0200

    Added laier severn WLD

---
+++ types/laiier/severnWLD/uplink.js
@@ -0,0 +1,64 @@
+function consume(event) {
+  const payload = event.data.payloadHex;
+  const bits = Bits.hexToBits(payload);
+  const { port } = event.data;
+  const data = {};
+  let topic = "default";
+
+  switch (port) {
+    case 1 || 99 || 102:
+      switch (port) {
+        case 1:
+          data.messageType = "REGULAR_MESSAGE";
+          break;
+        case 99:
+          data.messageType = "EMERGENCY_MESSAGE";
+          break;
+        case 102:
+          data.messageType = "SELF_TEST_MESSAGE";
+          break;
+        default:
+          break;
+      }
+
+      data.leakElectrode12 = !!Bits.bitsToUnsigned(bits.substr(0, 1));
+      data.leakElectrode11 = !!Bits.bitsToUnsigned(bits.substr(1, 1));
+      data.leakElectrode10 = !!Bits.bitsToUnsigned(bits.substr(2, 1));
+      data.leakElectrode9 = !!Bits.bitsToUnsigned(bits.substr(3, 1));
+
+      data.selfTestFailed = !!Bits.bitsToUnsigned(bits.substr(6, 1));
+      data.criticalWetFlag = !!Bits.bitsToUnsigned(bits.substr(7, 1));
+
+      data.leakElectrode8 = !!Bits.bitsToUnsigned(bits.substr(8, 1));
+      data.leakElectrode7 = !!Bits.bitsToUnsigned(bits.substr(9, 1));
+      data.leakElectrode6 = !!Bits.bitsToUnsigned(bits.substr(10, 1));
+      data.leakElectrode5 = !!Bits.bitsToUnsigned(bits.substr(11, 1));
+      data.leakElectrode4 = !!Bits.bitsToUnsigned(bits.substr(12, 1));
+      data.leakElectrode3 = !!Bits.bitsToUnsigned(bits.substr(13, 1));
+      data.leakElectrode2 = !!Bits.bitsToUnsigned(bits.substr(14, 1));
+      data.leakElectrode1 = !!Bits.bitsToUnsigned(bits.substr(15, 1));
+
+      data.accX = Hex.hexLittleEndianToBigEndian(payload.substr(4, 2), true); //  1/63 g
+      data.accY = Hex.hexLittleEndianToBigEndian(payload.substr(6, 2), true); //  1/63 g
+      data.accZ = Hex.hexLittleEndianToBigEndian(payload.substr(8, 2), true); //  1/63 g
+
+      data.temperature = Bits.bitsToSigned(bits.substr(48, 8));
+      data.wetnessThreshold = Bits.bitsToUnsigned(bits.substr(56, 8));
+      data.interval = Bits.bitsToUnsigned(bits.substr(64, 8));
+
+      break;
+    case 100:
+      topic = "start_up";
+      data.serialNumber = Bits.bitsToUnsigned(bits.substr(0, 64));
+      data.firmwareVersion = `${Bits.bitsToUnsigned(
+        bits.substr(64, 8),
+      )}.${Bits.bitsToUnsigned(bits.substr(72, 8))}.${Bits.bitsToUnsigned(
+        bits.substr(80, 8),
+      )}`;
+      break;
+    default:
+      break;
+  }
+
+  emit("sample", { data, topic });
+}
---
+++ types/laiier/severnWLD/uplink.spec.js
@@ -0,0 +1,180 @@
+const chai = require("chai");
+
+const rewire = require("rewire");
+const utils = require("test-utils");
+
+const { assert } = chai;
+
+describe("Laiier severnWLD Uplink", () => {
+  let measurementSchema = null;
+  let consume = null;
+  before((done) => {
+    const script = rewire("./uplink.js");
+    consume = utils.init(script);
+    utils
+      .loadSchema(`${__dirname}/measurement.schema.json`)
+      .then((parsedSchema) => {
+        measurementSchema = parsedSchema;
+        done();
+      });
+  });
+
+  let invalidMeasurementSchema = null;
+  before((done) => {
+    utils
+      .loadSchema(`${__dirname}/invalid_measurement.schema.json`)
+      .then((parsedSchema) => {
+        invalidMeasurementSchema = parsedSchema;
+        done();
+      });
+  });
+
+  let lifecycleSchema = null;
+  before((done) => {
+    utils
+      .loadSchema(`${__dirname}/lifecycle.schema.json`)
+      .then((parsedSchema) => {
+        lifecycleSchema = parsedSchema;
+        done();
+      });
+  });
+
+  let statusSchema = null;
+  before((done) => {
+    utils.loadSchema(`${__dirname}/status.schema.json`).then((parsedSchema) => {
+      statusSchema = parsedSchema;
+      done();
+    });
+  });
+
+  let errorSchema = null;
+  before((done) => {
+    utils.loadSchema(`${__dirname}/error.schema.json`).then((parsedSchema) => {
+      errorSchema = parsedSchema;
+      done();
+    });
+  });
+
+  describe("consume()", () => {
+    it("should decode Adnexo ax-dist measurement payload", () => {
+      const data = {
+        device: {
+          customFields: {
+            containerHeight: 600,
+            installationOffset: 0,
+          },
+        },
+        data: {
+          port: 100,
+          payloadHex: "340Adc008A0C",
+        },
+      };
+
+      utils.expectEmits((type, value) => {
+        assert.equal(type, "sample");
+        assert.isNotNull(value);
+        assert.typeOf(value.data, "object");
+
+        assert.equal(value.topic, "lifecycle");
+
+        assert.equal(value.data.batteryVoltage, 3.21);
+        assert.equal(value.data.batteryLevel, 100);
+
+        utils.validateSchema(value.data, lifecycleSchema, {
+          throwError: true,
+        });
+      });
+
+      utils.expectEmits((type, value) => {
+        assert.equal(type, "sample");
+        assert.isNotNull(value);
+        assert.typeOf(value.data, "object");
+
+        assert.equal(value.topic, "measurement");
+
+        assert.equal(value.data.distance, 261.2);
+        assert.equal(value.data.fillLevel, 56);
+        assert.equal(value.data.measurementType, "REGULAR_MEASUREMENT");
+        assert.equal(value.data.temperature, 22);
+
+        utils.validateSchema(value.data, measurementSchema, {
+          throwError: true,
+        });
+      });
+
+      consume(data);
+    });
+
+    it("should decode Adnexo ax-dist invalid measurement payload", () => {
+      const data = {
+        data: {
+          port: 104,
+          payloadHex: "340A7102dc008A0C",
+        },
+      };
+
+      utils.expectEmits((type, value) => {
+        assert.equal(type, "sample");
+        assert.isNotNull(value);
+        assert.typeOf(value.data, "object");
+
+        assert.equal(value.topic, "invalid_measurement");
+        assert.equal(value.data.measurementVariance, 62.5);
+
+        utils.validateSchema(value.data, invalidMeasurementSchema, {
+          throwError: true,
+        });
+      });
+
+      consume(data);
+    });
+
+    it("should decode Adnexo ax-dist error payload", () => {
+      const data = {
+        data: {
+          port: 5,
+          payloadHex: "045802000005060600a30200",
+        },
+      };
+
+      utils.expectEmits((type, value) => {
+        assert.equal(type, "sample");
+        assert.isNotNull(value);
+        assert.typeOf(value.data, "object");
+
+        assert.equal(value.topic, "error");
+        assert.equal(value.data.errorOn, "PARAM_READ");
+        assert.equal(value.data.errorCode, "PARAM_NOT_READABLE");
+
+        utils.validateSchema(value.data, errorSchema, { throwError: true });
+      });
+
+      consume(data);
+    });
+
+    it("should decode Adnexo ax-dist config payload", () => {
+      const data = {
+        data: {
+          port: 4,
+          payloadHex: "045802000005060600a30200",
+        },
+      };
+
+      utils.expectEmits((type, value) => {
+        assert.equal(type, "sample");
+        assert.isNotNull(value);
+        assert.typeOf(value.data, "object");
+
+        assert.equal(value.topic, "status");
+        assert.equal(value.data.joinInterval, 172800);
+        assert.equal(value.data.measurementInterval, 600);
+        assert.equal(value.data.payloadType, "REPORT_CONFIGURATION");
+        assert.equal(value.data.sendInterval, 6);
+
+        utils.validateSchema(value.data, statusSchema, { throwError: true });
+      });
+
+      consume(data);
+    });
+  });
+});
