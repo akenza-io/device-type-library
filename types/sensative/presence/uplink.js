07c564a33358315dd4e344ab9368178dc9271150
j2 <jonasg@akenza.com>
2023-11-22 16:47:58 +0100

    Added ms-drip, ms-drip-oil and ms-comfort. Updated old uplink and added new urls for existing strips

--- types/sensative/presence/uplink.js
+++ types/sensative/presence/uplink.js
@@ -1,202 +1,150 @@
-function decode(bytes, port) {
-  // Decode an uplink message from a buffer
-  // (array) of bytes to an object of fields.
+function decodeFrame(bytes, type, pos) {
+  const data = {};
+  let pointer = pos;
 
-  const decodeFrame = (type, target) => {
-    switch (type & 0x7f) {
-      case 0:
-        target.emptyFrame = {};
-        break;
-      case 1: // Battery 1byte 0-100%
-        target.batteryLevel = bytes[pos++];
-        break;
-      case 2: // TempReport 2bytes 0.1degree C
-        target.temperature =
-          ((bytes[pos] & 0x80 ? 0xffff << 16 : 0) |
-            (bytes[pos++] << 8) |
-            bytes[pos++]) /
-          10;
-        break;
-      case 3:
-        // Temp alarm
-        target.highAlarm = !!(bytes[pos] & 0x01); // boolean
-        target.lowAlarm = !!(bytes[pos] & 0x02); // boolean
-        pos++;
-        break;
-      case 4: // AvgTempReport 2bytes 0.1degree C
-        target.averageTemperature =
-          ((bytes[pos] & 0x80 ? 0xffff << 16 : 0) |
-            (bytes[pos++] << 8) |
-            bytes[pos++]) /
-          10;
-        break;
-      case 5:
-        // AvgTemp alarm
-        target.highAlarm = !!(bytes[pos] & 0x01); // boolean
-        target.lowAlarm = !!(bytes[pos] & 0x02); // boolean
-        pos++;
-        break;
-      case 6: // Humidity 1byte 0-100% in 0.5%
-        target.humidity = bytes[pos++] / 2; // relativeHumidity percent 0,5
-        break;
-      case 7: // Lux 2bytes 0-65535lux
-        target.lux = (bytes[pos++] << 8) | bytes[pos++]; // you can  the lux range between two sets (lux1 and 2)
-        break;
-      case 8: // Lux 2bytes 0-65535lux
-        target.lux2 = (bytes[pos++] << 8) | bytes[pos++];
-        break;
-      case 9: // DoorSwitch 1bytes binary
-        target.open = !!bytes[pos++]; // true = door open, false = door closed
-        break;
-      case 10: // DoorAlarm 1bytes binary
-        target.doorAlarm = !!bytes[pos++]; // boolean true = alarm
-        break;
-      case 11: // TamperReport 1bytes binary
-        target.tamperReport = !!bytes[pos++]; // should never trigger anymore
-        break;
-      case 12: // TamperAlarm 1bytes binary
-        target.tamperAlarm = !!bytes[pos++]; // should never trigger anymore
-        break;
-      case 13: // Flood 1byte 0-100%
-        target.flood = bytes[pos++]; // percentage, relative wetness
-        break;
-      case 14: // FloodAlarm 1bytes binary
-        target.floodAlarm = {};
-        target.floodAlarm = !!bytes[pos++]; // boolean, after >x<
-        break;
-      case 15: // oilAlarm 1bytes analog
-        target.oilAlarm = bytes[pos++];
-        target.foilAlarm = !!bytes[pos++];
-        break;
-      case 16: // UserSwitch1Alarm, 1 byte digital
-        target.userSwitch1Alarm = !!bytes[pos++];
-        break;
-      case 17: // DoorCountReport, 2 byte analog
-        target.doorCount = (bytes[pos++] << 8) | bytes[pos++];
-        break;
-      case 18: // PresenceReport, 1 byte digital
-        target.presence = !!bytes[pos++];
-        break;
-      case 19: // IRProximityReport
-        target.IRproximity = (bytes[pos++] << 8) | bytes[pos++];
-        break;
-      case 20: // IRCloseProximityReport, low power
-        target.IRcloseproximity = (bytes[pos++] << 8) | bytes[pos++];
-        break;
-      case 21: // CloseProximityAlarm, something very close to presence sensor
-        target.closeProximityAlarm = !!bytes[pos++];
-        break;
-      case 22: // DisinfectAlarm
-        target.disinfectAlarm = bytes[pos++];
-        if (target.disinfectAlarm === 0) {
-          target.disinfectAlarm.state = "DIRTY";
-        }
-        if (target.disinfectAlarm === 1) {
-          target.disinfectAlarm.state = "OCCUPIED";
-        }
-        if (target.disinfectAlarm === 2) {
-          target.disinfectAlarm.state = "CLEANING";
-        }
-        if (target.disinfectAlarm === 3) {
-          target.disinfectAlarm.state = "CLEAN";
-        }
-        break;
-      case 80:
-        target.humidity = bytes[pos++] / 2;
-        target.temperature =
-          ((bytes[pos] & 0x80 ? 0xffff << 16 : 0) |
-            (bytes[pos++] << 8) |
-            bytes[pos++]) /
-          10;
-        break;
-      case 81:
-        target.humidity = bytes[pos++] / 2;
-        target.averageTemperature =
-          ((bytes[pos] & 0x80 ? 0xffff << 16 : 0) |
-            (bytes[pos++] << 8) |
-            bytes[pos++]) /
-          10;
-        break;
-      case 82:
-        target.open = !!bytes[pos++]; // true = door open, false = door closed
-        target.temperature =
-          ((bytes[pos] & 0x80 ? 0xffff << 16 : 0) |
-            (bytes[pos++] << 8) |
-            bytes[pos++]) /
-          10;
-        break;
-      case 112: // Capacitance Raw Sensor Value 2bytes 0-65535
-        target.capacitanceFlood = (bytes[pos++] << 8) | bytes[pos++]; // should never trigger anymore
-        break;
-      case 113: // Capacitance Raw Sensor Value 2bytes 0-65535
-        target.capacitancePad = (bytes[pos++] << 8) | bytes[pos++]; // should never trigger anymore
-        break;
-      case 110:
-        const number =
-          ((bytes[pos++] << 24) |
-            (bytes[pos++] << 16) |
-            (bytes[pos++] << 8) |
-            bytes[pos++]) >>>
-          0;
-        target.softwareVersion = number.toString(16);
-        target.startUpCount = bytes[pos++];
-        target.watchdogCount = bytes[pos++];
-        target.stackTxFailRebootCount = bytes[pos++];
-        target.badConditionsCounter = bytes[pos++];
-        break;
-      case 114: // Capacitance Raw Sensor Value 2bytes 0-65535
-        target.capacitanceEnd = (bytes[pos++] << 8) | bytes[pos++]; // should never trigger anymore
-        break;
-      default:
-    }
-  };
-
-  const decoded = {};
-  var pos = 0;
-  let type;
-  switch (port) {
-    case 1:
-      if (bytes.length < 2) {
-        decoded.error = "Wrong length of RX package";
-        break;
-      }
-      decoded.historySeqNr = (bytes[pos++] << 8) | bytes[pos++];
-      decoded.prevHistSeqNr = decoded.historySeqNr;
-      while (pos < bytes.length) {
-        type = bytes[pos++];
-        if (type & 0x80) {
-          decoded.prevHistSeqNr--;
-        }
-        decodeFrame(type, decoded);
-      }
+  switch (type & 0x7f) {
+    case 0:
+      data.emptyFrame = {};
       break;
-    case 2:
-      var now = new Date();
-      decoded.history = {};
-      if (bytes.length < 2) {
-        decoded.error = "Wrong length of RX package";
-        break;
-      }
-      var seqNr = (bytes[pos++] << 8) | bytes[pos++];
-      while (pos < bytes.length) {
-        decoded.history[seqNr] = {};
-        decoded.history.now = now.toUTCString();
-        const secondsAgo =
-          (bytes[pos++] << 24) |
-          (bytes[pos++] << 16) |
-          (bytes[pos++] << 8) |
-          bytes[pos++];
-        decoded.history[seqNr].timeStamp = new Date(
-          now.getTime() - secondsAgo * 1000,
-        ).toUTCString();
-        type = bytes[pos++];
-        decodeFrame(type, decoded.history[seqNr]);
-        seqNr++;
-      }
+    case 1: // Battery 1byte 0-100%
+      data.batteryLevel = bytes[pointer++];
+      break;
+    case 2: // TempReport 2bytes 0.1degree C
+      // celcius 0.1 precision
+      data.temperature =
+        ((bytes[pointer] & 0x80 ? 0xffff << 16 : 0) |
+          (bytes[pointer++] << 8) |
+          bytes[pointer++]) /
+        10;
+      break;
+    case 3:
+      // Temp alarm
+      // sends alarm after >x<
+      data.highAlarm = !!(bytes[pointer] & 0x01); // boolean
+      data.lowAlarm = !!(bytes[pointer] & 0x02); // boolean
+      pointer++;
+      break;
+    case 4: // AvgTempReport 2bytes 0.1degree C
+      data.averageTemperature =
+        ((bytes[pointer] & 0x80 ? 0xffff << 16 : 0) |
+          (bytes[pointer++] << 8) |
+          bytes[pointer++]) /
+        10;
+      break;
+    case 5:
+      // AvgTemp alarm
+      // sends alarm after >x<
+      data.highAlarm = !!(bytes[pointer] & 0x01); // boolean
+      data.lowAlarm = !!(bytes[pointer] & 0x02); // boolean
+      pointer++;
+      break;
+    case 6: // Humidity 1byte 0-100% in 0.5%
+      data.humidity = bytes[pointer] / 2; // relativeHumidity percent 0,5
+      break;
+    case 7: // Lux 2bytes 0-65535lux
+      data.light = (bytes[pointer++] << 8) | bytes[pointer++]; // you can  the lux range between two sets (lux1 and 2)
+      break;
+    case 8: // Lux 2bytes 0-65535lux
+      data.light2 = (bytes[pointer++] << 8) | bytes[pointer++];
+      break;
+    case 9: // DoorSwitch 1bytes binary
+      data.closed = !!bytes[pointer++]; // true = door closed, false = door open
+      break;
+    case 10: // DoorAlarm 1bytes binary
+      data.doorAlarm = !!bytes[pointer++]; // boolean true = alarm
+      break;
+    case 11: // TamperReport 1bytes binary (was previously TamperSwitch)
+      data.tamperReport = !!bytes[pointer++];
+      break;
+    case 12: // TamperAlarm 1bytes binary
+      data.tamperAlarm = !!bytes[pointer++];
+      break;
+    case 13: // Flood 1byte 0-100%
+      data.flood = bytes[pointer++]; // percentage, relative wetness
+      break;
+    case 14: // FloodAlarm 1bytes binary
+      data.floodAlarm = !!bytes[pointer++]; // boolean, after >x<
+      break;
+    case 15: // FoilAlarm 1bytes binary
+      data.oilAlarm = !!bytes[pointer];
+      data.foilAlarm = !!bytes[pointer++];
+      break;
+    case 16: // UserSwitch1Alarm, 1 byte digital
+      data.userSwitchAlarm = !!bytes[pointer++];
+      break;
+    case 17: // DoorCountReport, 2 byte analog
+      data.doorCount = (bytes[pointer++] << 8) | bytes[pointer++];
+      break;
+    case 18: // PresenceReport, 1 byte digital
+      data.presence = !!bytes[pointer++];
+      break;
+    case 19: // IRProximityReport
+      data.irProximity = (bytes[pointer++] << 8) | bytes[pointer++];
+      break;
+    case 20: // IRCloseProximityReport, low power
+      data.irCloseProximity = (bytes[pointer++] << 8) | bytes[pointer++];
+      break;
+    case 21: // CloseProximityAlarm, something very close to presence sensor
+      data.closeProximityAlarm = !!bytes[pointer++];
+      break;
+    case 22: // DisinfectAlarm
+      data.disinfectAlarm = bytes[pointer++];
+      if (data.disinfectAlarm === 0) data.disinfectAlarm = "DIRTY";
+      if (data.disinfectAlarm === 1) data.disinfectAlarm = "OCCUPIED";
+      if (data.disinfectAlarm === 2) data.disinfectAlarm = "CLEANING";
+      if (data.disinfectAlarm === 3) data.disinfectAlarm = "CLEAN";
+      break;
+    case 80:
+      data.humidity = bytes[pointer++] / 2;
+      data.temperature =
+        ((bytes[pointer] & 0x80 ? 0xffff << 16 : 0) |
+          (bytes[pointer++] << 8) |
+          bytes[pointer++]) /
+        10;
+      break;
+    case 81:
+      data.humidity = bytes[pointer++] / 2;
+      data.averageTemperature =
+        ((bytes[pointer] & 0x80 ? 0xffff << 16 : 0) |
+          (bytes[pointer++] << 8) |
+          bytes[pointer++]) /
+        10;
+      break;
+    case 82:
+      data.closed = !!bytes[pointer++]; // true = door open, false = door closed // Inverted in example decoder
+      data.temperature =
+        ((bytes[pointer] & 0x80 ? 0xffff << 16 : 0) |
+          (bytes[pointer++] << 8) |
+          bytes[pointer++]) /
+        10;
+      break;
+    case 112: // Capacitance Raw Sensor Value 2bytes 0-65535
+      data.capacitanceFlood = (bytes[pointer++] << 8) | bytes[pointer++]; // should never trigger anymore
+      break;
+    case 113: // Capacitance Raw Sensor Value 2bytes 0-65535
+      data.capacitancePad = (bytes[pointer++] << 8) | bytes[pointer++]; // should never trigger anymore
+      break;
+    case 110: {
+      const number =
+        ((bytes[pointer++] << 24) |
+          (bytes[pointer++] << 16) |
+          (bytes[pointer++] << 8) |
+          bytes[pointer++]) >>>
+        0;
+      data.softwareVersion = number.toString(16);
+      data.startUpCount = bytes[pointer++];
+      data.watchdogCount = bytes[pointer++];
+      data.stackTxFailRebootCount = bytes[pointer++];
+      data.badConditionsCounter = bytes[pointer++];
+      break;
+    }
+    case 114: // Capacitance Raw Sensor Value 2bytes 0-65535
+      data.capacitanceEnd = (bytes[pointer++] << 8) | bytes[pointer++]; // should never trigger anymore
       break;
     default:
+      break;
   }
-  return decoded;
+  return { data, pointer };
 }
 
 function deleteUnusedKeys(data) {
@@ -211,21 +159,59 @@ function deleteUnusedKeys(data) {
   return keysRetained;
 }
 
-function parseHexString(str) {
-  const result = [];
-  while (str.length >= 2) {
-    result.push(parseInt(str.substring(0, 2), 16));
-    str = str.substring(2, str.length);
-  }
-
-  return result;
-}
-
 function consume(event) {
   const payload = event.data.payloadHex;
   const { port } = event.data;
-  const bytes = parseHexString(payload);
-  const decoded = decode(bytes, port);
+  const bytes = Hex.hexToBytes(payload);
+
+  const decoded = {};
+  let pos = 2;
+  let type;
+
+  switch (port) {
+    case 1:
+      if (bytes.length < 2) {
+        emit("log", { error: "Wrong length of RX package" });
+        break;
+      }
+      while (pos < bytes.length) {
+        type = bytes[pos++];
+        const decodedFrame = decodeFrame(bytes, type, pos);
+        pos = decodedFrame.pointer;
+        Object.assign(decoded, decodedFrame.data);
+      }
+      break;
+
+    case 2: {
+      const now = new Date();
+      decoded.history = {};
+      if (bytes.length < 2) {
+        decoded.history.error = "Wrong length of RX package";
+        break;
+      }
+      let seqNr = (bytes[pos++] << 8) | bytes[pos++];
+      while (pos < bytes.length) {
+        decoded.history[seqNr] = {};
+        decoded.history.now = now.toUTCString();
+        const secondsAgo =
+          (bytes[pos++] << 24) |
+          (bytes[pos++] << 16) |
+          (bytes[pos++] << 8) |
+          bytes[pos++];
+        decoded.history[seqNr].timeStamp = new Date(
+          now.getTime() - secondsAgo * 1000,
+        ).toUTCString();
+        type = bytes[pos++];
+        const decodedFrame = decodeFrame(bytes, type, pos);
+        pos = decodedFrame.pointer;
+        Object.assign(decoded, decodedFrame.data);
+        seqNr++;
+      }
+      break;
+    }
+    default:
+      break;
+  }
 
   const def = {};
   def.temperature = decoded.temperature;
@@ -233,7 +219,7 @@ function consume(event) {
   def.humidity = decoded.humidity;
   def.light = decoded.light;
   def.light2 = decoded.light2;
-  def.open = decoded.open;
+  def.closed = decoded.closed;
   def.tamperReport = decoded.tamperReport;
   def.doorCount = decoded.doorCount;
   def.presence = decoded.presence;
@@ -246,18 +232,24 @@ function consume(event) {
   alarm.doorAlarm = decoded.doorAlarm;
   alarm.tamperAlarm = decoded.tamperAlarm;
   alarm.floodAlarm = decoded.floodAlarm;
+  alarm.oilAlarm = decoded.oilAlarm;
   alarm.foilAlarm = decoded.foilAlarm;
   alarm.userSwitchAlarm = decoded.userSwitchAlarm;
   alarm.closeProximityAlarm = decoded.closeProximityAlarm;
   alarm.disinfectAlarm = decoded.disinfectAlarm;
 
   const lifecycle = {};
-  lifecycle.batteryLevel = decoded.batteryLevel;
+  if (decoded.batteryLevel !== 0) {
+    lifecycle.batteryLevel = decoded.batteryLevel;
+  }
+  lifecycle.error = decoded.error;
   lifecycle.softwareVersion = decoded.softwareVersion;
   lifecycle.startUpCount = decoded.startUpCount;
   lifecycle.watchdogCount = decoded.watchdogCount;
   lifecycle.stackTxFailRebootCount = decoded.stackTxFailRebootCount;
   lifecycle.badConditionsCounter = decoded.badConditionsCounter;
+  lifecycle.historySeqNr = decoded.historySeqNr;
+  lifecycle.prevHistSeqNr = decoded.prevHistSeqNr;
 
   if (deleteUnusedKeys(lifecycle)) {
     emit("sample", { data: lifecycle, topic: "lifecycle" });
