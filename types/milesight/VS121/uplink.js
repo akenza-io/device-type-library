56fe4244616bdc41d4783115cc9fe5df53b37108
j2 <jonasg@akenza.com>
2024-04-24 14:40:04 +0200

    Added new functionalities for the VS121 new Version. Update is backwardscompatible

--- types/milesight/VS121/uplink.js
+++ types/milesight/VS121/uplink.js
@@ -14,6 +14,12 @@ function readUInt16BE(bytes) {
   return value & 0xffff;
 }
 
+function readUInt32LE(bytes) {
+  const value =
+    (bytes[3] << 24) + (bytes[2] << 16) + (bytes[1] << 8) + bytes[0];
+  return (value & 0xffffffff) >>> 0;
+}
+
 function readUInt16LE(bytes) {
   const value = (bytes[1] << 8) + bytes[0];
   return value & 0xffff;
@@ -49,37 +55,37 @@ function isEmpty(obj) {
 function consume(event) {
   const payload = event.data.payloadHex;
   const bytes = parseHexString(payload);
-
-  const decoded = {};
-  let topic = "default";
+  const lifecycle = {};
+  const regionCounter = {};
 
   for (let i = 0; i < bytes.length; ) {
     const channelId = bytes[i++];
     const channelType = bytes[i++];
+    const decoded = {};
 
     // PROTOCOL VESION
     if (channelId === 0xff && channelType === 0x01) {
-      decoded.protocolVersion = bytes[i];
+      lifecycle.protocolVersion = bytes[i];
       i += 1;
-      topic = "lifecycle";
     }
     // SERIAL NUMBER
     else if (channelId === 0xff && channelType === 0x08) {
-      decoded.sn = readString(Array.from(bytes).slice(i, i + 6));
+      lifecycle.sn = readString(Array.from(bytes).slice(i, i + 6));
       i += 6;
-      topic = "lifecycle";
     }
     // HARDWARE VERSION
     else if (channelId === 0xff && channelType === 0x09) {
-      decoded.hardwareVersion = readVersion(Array.from(bytes).slice(i, i + 2));
+      lifecycle.hardwareVersion = readVersion(
+        Array.from(bytes).slice(i, i + 2),
+      );
       i += 2;
-      topic = "lifecycle";
     }
     // FIRMWARE VERSION
     else if (channelId === 0xff && channelType === 0x0a) {
-      decoded.firmwareVersion = readVersion(Array.from(bytes).slice(i, i + 4));
+      lifecycle.firmwareVersion = readVersion(
+        Array.from(bytes).slice(i, i + 4),
+      );
       i += 4;
-      topic = "lifecycle";
     }
     // PEOPLE COUNTER
     else if (channelId === 0x04 && channelType === 0xc9) {
@@ -91,26 +97,114 @@ function consume(event) {
         decoded[tmp] = (region >> idx) & 1;
       }
       i += 4;
-      topic = "count";
+      emit("sample", { data: decoded, topic: "count" });
     }
     // PEOPLE IN/OUT
     else if (channelId === 0x05 && channelType === 0xcc) {
       decoded.in = readInt16LE(Array.from(bytes).slice(i, i + 2));
       decoded.out = readInt16LE(Array.from(bytes).slice(i + 2, i + 4));
       i += 4;
-      topic = "people";
+      emit("sample", { data: decoded, topic: "people" });
     }
     // PEOPLE MAX
     else if (channelId === 0x06 && channelType === 0xcd) {
       decoded.peopleMax = bytes[i];
       i += 1;
-      topic = "people_max";
+      emit("sample", { data: decoded, topic: "people_max" });
+    }
+    // REGION COUNTER
+    else if (channelId === 0x07 && channelType === 0xd5) {
+      regionCounter.region1count = bytes[i];
+      regionCounter.region2count = bytes[i + 1];
+      regionCounter.region3count = bytes[i + 2];
+      regionCounter.region4count = bytes[i + 3];
+      regionCounter.region5count = bytes[i + 4];
+      regionCounter.region6count = bytes[i + 5];
+      regionCounter.region7count = bytes[i + 6];
+      regionCounter.region8count = bytes[i + 7];
+      i += 8;
+    }
+    // REGION COUNTER
+    else if (channelId === 0x08 && channelType === 0xd5) {
+      regionCounter.region9count = bytes[i];
+      regionCounter.region10count = bytes[i + 1];
+      regionCounter.region11count = bytes[i + 2];
+      regionCounter.region12count = bytes[i + 3];
+      regionCounter.region13count = bytes[i + 4];
+      regionCounter.region14count = bytes[i + 5];
+      regionCounter.region15count = bytes[i + 6];
+      regionCounter.region16count = bytes[i + 7];
+      i += 8;
+    }
+    // A FLOW
+    else if (channelId === 0x09 && channelType === 0xda) {
+      decoded.aToA = readUInt16LE(bytes.slice(i, i + 2));
+      decoded.aToB = readUInt16LE(bytes.slice(i + 2, i + 4));
+      decoded.aToC = readUInt16LE(bytes.slice(i + 4, i + 6));
+      decoded.aToD = readUInt16LE(bytes.slice(i + 6, i + 8));
+      i += 8;
+      emit("sample", { data: decoded, topic: "a_flow" });
+    }
+    // B FLOW
+    else if (channelId === 0x0a && channelType === 0xda) {
+      decoded.bToA = readUInt16LE(bytes.slice(i, i + 2));
+      decoded.bToB = readUInt16LE(bytes.slice(i + 2, i + 4));
+      decoded.bToC = readUInt16LE(bytes.slice(i + 4, i + 6));
+      decoded.bToD = readUInt16LE(bytes.slice(i + 6, i + 8));
+      i += 8;
+      emit("sample", { data: decoded, topic: "b_flow" });
+    }
+    // C FLOW
+    else if (channelId === 0x0b && channelType === 0xda) {
+      decoded.cToA = readUInt16LE(bytes.slice(i, i + 2));
+      decoded.cToB = readUInt16LE(bytes.slice(i + 2, i + 4));
+      decoded.cToC = readUInt16LE(bytes.slice(i + 4, i + 6));
+      decoded.cToD = readUInt16LE(bytes.slice(i + 6, i + 8));
+      i += 8;
+      emit("sample", { data: decoded, topic: "c_flow" });
+    }
+    // D FLOW
+    else if (channelId === 0x0c && channelType === 0xda) {
+      decoded.dToA = readUInt16LE(bytes.slice(i, i + 2));
+      decoded.dToB = readUInt16LE(bytes.slice(i + 2, i + 4));
+      decoded.dToC = readUInt16LE(bytes.slice(i + 4, i + 6));
+      decoded.dToD = readUInt16LE(bytes.slice(i + 6, i + 8));
+      i += 8;
+      emit("sample", { data: decoded, topic: "d_flow" });
+    }
+    // TOTAL IN/OUT
+    else if (channelId === 0x0d && channelType === 0xcc) {
+      decoded.peopleTotalIn = readUInt16LE(bytes.slice(i, i + 2));
+      decoded.peopleTotalOut = readUInt16LE(bytes.slice(i + 2, i + 4));
+      i += 4;
+      emit("sample", { data: decoded, topic: "total" });
+    }
+    // DWELL TIME
+    else if (channelId === 0x0e && channelType === 0xe4) {
+      decoded.region = bytes[i];
+      decoded.dwellTimeAvg = readUInt16LE(bytes.slice(i + 1, i + 3));
+      decoded.dwellTimeMax = readUInt16LE(bytes.slice(i + 3, i + 5));
+      i += 5;
+      emit("sample", { data: decoded, topic: "dwell_time" });
+    }
+    // TIMESTAMP
+    else if (channelId === 0x0f && channelType === 0x85) {
+      /*
+      // We probably do not need that atm
+      decoded.timestamp = readUInt32LE(bytes.slice(i, i + 4));
+      i += 4;
+      emit("sample", { data: decoded, topic: "timestamp" });
+      */
     } else {
       break;
     }
   }
 
-  if (!isEmpty(decoded)) {
-    emit("sample", { data: decoded, topic });
+  if (!isEmpty(lifecycle)) {
+    emit("sample", { data: lifecycle, topic: "lifecycle" });
+  }
+
+  if (!isEmpty(regionCounter)) {
+    emit("sample", { data: regionCounter, topic: "region_counter" });
   }
 }
--- types/milesight/VS121/uplink.spec.js
+++ types/milesight/VS121/uplink.spec.js
@@ -28,6 +28,48 @@ describe("VS121 Uplink", () => {
     });
   });
 
+  let aFlowSchema = null;
+  before((done) => {
+    utils.loadSchema(`${__dirname}/a_flow.schema.json`).then((parsedSchema) => {
+      aFlowSchema = parsedSchema;
+      done();
+    });
+  });
+
+  let bFlowSchema = null;
+  before((done) => {
+    utils.loadSchema(`${__dirname}/b_flow.schema.json`).then((parsedSchema) => {
+      bFlowSchema = parsedSchema;
+      done();
+    });
+  });
+
+  let cFlowSchema = null;
+  before((done) => {
+    utils.loadSchema(`${__dirname}/c_flow.schema.json`).then((parsedSchema) => {
+      cFlowSchema = parsedSchema;
+      done();
+    });
+  });
+
+  let dFlowSchema = null;
+  before((done) => {
+    utils.loadSchema(`${__dirname}/d_flow.schema.json`).then((parsedSchema) => {
+      dFlowSchema = parsedSchema;
+      done();
+    });
+  });
+
+  let regionCounterSchema = null;
+  before((done) => {
+    utils
+      .loadSchema(`${__dirname}/region_counter.schema.json`)
+      .then((parsedSchema) => {
+        regionCounterSchema = parsedSchema;
+        done();
+      });
+  });
+
   let peopleSchema = null;
   before((done) => {
     utils.loadSchema(`${__dirname}/people.schema.json`).then((parsedSchema) => {
@@ -133,6 +175,121 @@ describe("VS121 Uplink", () => {
         assert.equal(value.data.peopleMax, 5);
 
         utils.validateSchema(value.data, peopleMaxSchema, { throwError: true });
+      });
+
+      consume(data);
+    });
+
+    it("should decode should decode the VS121 regionCount payload", () => {
+      const data = {
+        data: {
+          port: 1,
+          payloadHex: "07D5000100000000000308D50100000000000000",
+        },
+      };
+
+      utils.expectEmits((type, value) => {
+        assert.equal(type, "sample");
+        assert.isNotNull(value);
+        assert.typeOf(value.data, "object");
+
+        assert.equal(value.topic, "region_counter");
+        assert.equal(value.data.region1count, 0);
+        assert.equal(value.data.region2count, 1);
+        assert.equal(value.data.region3count, 0);
+        assert.equal(value.data.region4count, 0);
+        assert.equal(value.data.region5count, 0);
+        assert.equal(value.data.region6count, 0);
+        assert.equal(value.data.region7count, 0);
+        assert.equal(value.data.region8count, 3);
+        assert.equal(value.data.region9count, 1);
+        assert.equal(value.data.region10count, 0);
+        assert.equal(value.data.region11count, 0);
+        assert.equal(value.data.region12count, 0);
+        assert.equal(value.data.region13count, 0);
+        assert.equal(value.data.region14count, 0);
+        assert.equal(value.data.region15count, 0);
+        assert.equal(value.data.region16count, 0);
+
+        utils.validateSchema(value.data, regionCounterSchema, {
+          throwError: true,
+        });
+      });
+
+      consume(data);
+    });
+
+    it("should decode should decode the VS121 x to x flow payload", () => {
+      const data = {
+        data: {
+          port: 1,
+          payloadHex:
+            "09DA00010002000000000ADA00000000000000000BDA00000000000000120CDA0000000000000000",
+        },
+      };
+
+      utils.expectEmits((type, value) => {
+        assert.equal(type, "sample");
+        assert.isNotNull(value);
+        assert.typeOf(value.data, "object");
+
+        assert.equal(value.topic, "a_flow");
+        assert.equal(value.data.aToA, 256);
+        assert.equal(value.data.aToB, 512);
+        assert.equal(value.data.aToC, 0);
+        assert.equal(value.data.aToD, 0);
+
+        utils.validateSchema(value.data, aFlowSchema, {
+          throwError: true,
+        });
+      });
+
+      utils.expectEmits((type, value) => {
+        assert.equal(type, "sample");
+        assert.isNotNull(value);
+        assert.typeOf(value.data, "object");
+
+        assert.equal(value.topic, "b_flow");
+        assert.equal(value.data.bToA, 0);
+        assert.equal(value.data.bToB, 0);
+        assert.equal(value.data.bToC, 0);
+        assert.equal(value.data.bToD, 0);
+
+        utils.validateSchema(value.data, bFlowSchema, {
+          throwError: true,
+        });
+      });
+
+      utils.expectEmits((type, value) => {
+        assert.equal(type, "sample");
+        assert.isNotNull(value);
+        assert.typeOf(value.data, "object");
+
+        assert.equal(value.topic, "c_flow");
+        assert.equal(value.data.cToA, 0);
+        assert.equal(value.data.cToB, 0);
+        assert.equal(value.data.cToC, 0);
+        assert.equal(value.data.cToD, 4608);
+
+        utils.validateSchema(value.data, cFlowSchema, {
+          throwError: true,
+        });
+      });
+
+      utils.expectEmits((type, value) => {
+        assert.equal(type, "sample");
+        assert.isNotNull(value);
+        assert.typeOf(value.data, "object");
+
+        assert.equal(value.topic, "d_flow");
+        assert.equal(value.data.dToA, 0);
+        assert.equal(value.data.dToB, 0);
+        assert.equal(value.data.dToC, 0);
+        assert.equal(value.data.dToD, 0);
+
+        utils.validateSchema(value.data, dFlowSchema, {
+          throwError: true,
+        });
       });
 
       consume(data);
