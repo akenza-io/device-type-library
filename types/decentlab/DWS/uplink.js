fadc9bd60ce165e67931dacbbe5d2d4975b3eaf4
j2 <jonasg@akenza.com>
2024-05-02 14:19:41 +0200

    Added Decentlab DWS

---
+++ types/decentlab/DWS/uplink.js
@@ -0,0 +1,173 @@
+const decentlab_decoder = {
+  PROTOCOL_VERSION: 2,
+  /* device-specific parameters */
+  PARAMETERS: {
+    f02: 232263168,
+    s: 0.000302459,
+    m0: 1370,
+  },
+  SENSORS: [
+    {
+      length: 3,
+      values: [
+        {
+          name: "frequency",
+          displayName: "Frequency",
+          convert(x) {
+            return (x[0] / x[1]) * 32768;
+          },
+          unit: "Hz",
+        },
+        {
+          name: "weight",
+          displayName: "Weight",
+          convert(x) {
+            return (
+              (Math.pow((x[0] / x[1]) * 32768, 2) - this.PARAMETERS.f02) *
+                this.PARAMETERS.s +
+              this.PARAMETERS.m0
+            );
+          },
+          unit: "g",
+        },
+      ],
+    },
+    {
+      length: 1,
+      values: [
+        {
+          name: "battery_voltage",
+          displayName: "Battery voltage",
+          convert(x) {
+            return x[0] / 1000;
+          },
+          unit: "V",
+        },
+      ],
+    },
+  ],
+
+  read_int(bytes, pos) {
+    return (bytes[pos] << 8) + bytes[pos + 1];
+  },
+
+  decode(msg) {
+    let bytes = msg;
+    let i;
+    let j;
+    if (typeof msg === "string") {
+      bytes = [];
+      for (i = 0; i < msg.length; i += 2) {
+        bytes.push(parseInt(msg.substring(i, i + 2), 16));
+      }
+    }
+
+    const version = bytes[0];
+    if (version !== this.PROTOCOL_VERSION) {
+      return { error: `protocol version ${version} doesn't match v2` };
+    }
+
+    const deviceId = this.read_int(bytes, 1);
+    let flags = this.read_int(bytes, 3);
+    const result = { protocol_version: version, device_id: deviceId };
+    // decode payload
+    let pos = 5;
+    for (i = 0; i < this.SENSORS.length; i++, flags >>= 1) {
+      if ((flags & 1) !== 1) {
+        continue;
+      }
+
+      const sensor = this.SENSORS[i];
+      const x = [];
+      // convert data to 16-bit integer array
+      for (j = 0; j < sensor.length; j++) {
+        x.push(this.read_int(bytes, pos));
+        pos += 2;
+      }
+
+      // decode sensor values
+      for (j = 0; j < sensor.values.length; j++) {
+        const value = sensor.values[j];
+        if ("convert" in value) {
+          result[value.name] = {
+            displayName: value.displayName,
+            value: value.convert.bind(this)(x),
+          };
+          if ("unit" in value) {
+            result[value.name].unit = value.unit;
+          }
+        }
+      }
+    }
+    return result;
+  },
+};
+
+function deleteUnusedKeys(data) {
+  let keysRetained = false;
+  Object.keys(data).forEach((key) => {
+    if (data[key] === undefined) {
+      delete data[key];
+    } else {
+      keysRetained = true;
+    }
+  });
+  return keysRetained;
+}
+
+function consume(event) {
+  const payload = event.data.payloadHex;
+  let customFieldsUsed = false;
+
+  if (event.device !== undefined && data.distance !== undefined) {
+    if (event.device.customFields !== undefined) {
+      const { customFields } = event.device;
+
+      if (
+        customFields.force !== undefined &&
+        customFields.underLoad !== undefined &&
+        customFields.strain !== undefined
+      ) {
+        const { force } = customFields; // F = m · 9.8067 [N]
+        const { underLoad } = customFields; // l = F · 1.5 [μm]
+        const { strain } = customFields; // s = l / 0.066 [μm/m
+        decentlab_decoder.PARAMETERS.f02 = force;
+        decentlab_decoder.PARAMETERS.s = underLoad;
+        decentlab_decoder.PARAMETERS.m0 = strain;
+
+        customFieldsUsed = true;
+      }
+    }
+  }
+
+  const sample = decentlab_decoder.decode(payload);
+  const data = {};
+  const lifecycle = {};
+
+  // Default values
+  data.frequency = sample.frequency;
+  data.weight = sample.weight;
+
+  // Lifecycle values
+  lifecycle.batteryVoltage = sample.battery_voltage;
+  lifecycle.protocolVersion = sample.protocol_version;
+  lifecycle.deviceId = sample.device_id;
+
+  if (deleteUnusedKeys(data) && customFieldsUsed) {
+    emit("sample", { data, topic: "default" });
+  }
+
+  if (!customFieldsUsed) {
+    emit("sample", {
+      data: {
+        error:
+          "Please define customfields: force, underLoad & strain. They should be defined on the sensor.",
+      },
+      topic: "error",
+    });
+  }
+
+  if (deleteUnusedKeys(lifecycle)) {
+    emit("sample", { data: lifecycle, topic: "lifecycle" });
+  }
+}
---
+++ types/decentlab/DWS/uplink.spec.js
@@ -0,0 +1,67 @@
+const chai = require("chai");
+
+const rewire = require("rewire");
+const utils = require("test-utils");
+
+const { assert } = chai;
+
+describe("Decentlab PR36 Uplink", () => {
+  let defaultSchema = null;
+  let consume = null;
+  before((done) => {
+    const script = rewire("./uplink.js");
+    consume = utils.init(script);
+    utils
+      .loadSchema(`${__dirname}/default.schema.json`)
+      .then((parsedSchema) => {
+        defaultSchema = parsedSchema;
+        done();
+      });
+  });
+
+  let lifecycleSchema = null;
+  before((done) => {
+    utils
+      .loadSchema(`${__dirname}/lifecycle.schema.json`)
+      .then((parsedSchema) => {
+        lifecycleSchema = parsedSchema;
+        done();
+      });
+  });
+
+  describe("consume()", () => {
+    it("should decode Decentlab PR36 payload", () => {
+      const data = {
+        data: {
+          payloadHex: "02032b0003806797810c2b",
+        },
+      };
+
+      utils.expectEmits((type, value) => {
+        assert.equal(type, "sample");
+        assert.isNotNull(value);
+        assert.typeOf(value.data, "object");
+
+        assert.equal(value.topic, "default");
+        assert.equal(value.data.pressure, 0.0125732421875);
+        assert.equal(value.data.temperature, 23.50390625);
+
+        utils.validateSchema(value.data, defaultSchema, { throwError: true });
+      });
+
+      utils.expectEmits((type, value) => {
+        assert.equal(type, "sample");
+        assert.isNotNull(value);
+        assert.typeOf(value.data, "object");
+
+        assert.equal(value.data.batteryVoltage, 3.115);
+        assert.equal(value.data.protocolVersion, 2);
+        assert.equal(value.data.deviceId, 811);
+
+        utils.validateSchema(value.data, lifecycleSchema, { throwError: true });
+      });
+
+      consume(data);
+    });
+  });
+});
