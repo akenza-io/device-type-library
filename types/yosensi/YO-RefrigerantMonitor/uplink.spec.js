e371fb005951c1618e74964b97bdcc462aa2123b
maciejSAC <maciej.sacewicz@yosensi.io>
2024-05-15 12:52:10 +0200

    Adding 3 yosensi devices to Akenza platform

---
+++ types/yosensi/YO-RefrigerantMonitor/uplink.spec.js
@@ -0,0 +1,121 @@
+const chai = require("chai");
+
+const rewire = require("rewire");
+const utils = require("test-utils");
+
+const { assert } = chai;
+
+describe("Yosensi YO Refrigerant Monitor uplink", () => {
+  let batteryVoltageSchema = null;
+  let consume = null;
+  before((done) => {
+    const script = rewire("./uplink.js");
+    consume = utils.init(script);
+    utils
+      .loadSchema(`${__dirname}/batteryVoltage.schema.json`)
+      .then((parsedSchema) => {
+        batteryVoltageSchema = parsedSchema;
+        done();
+      });
+  });
+
+  let internalTemperatureSchema = null;
+  before((done) => {
+    utils.loadSchema(`${__dirname}/internalTemperature.schema.json`).then((parsedSchema) => {
+      internalTemperatureSchema = parsedSchema;
+      done();
+    });
+  });
+
+  let temperatureSchema = null;
+  before((done) => {
+    utils.loadSchema(`${__dirname}/temperature.schema.json`).then((parsedSchema) => {
+      temperatureSchema = parsedSchema;
+      done();
+    });
+  });
+
+  let humiditySchema = null;
+  before((done) => {
+    utils.loadSchema(`${__dirname}/humidity.schema.json`).then((parsedSchema) => {
+      humiditySchema = parsedSchema;
+      done();
+    });
+  });
+
+  let pressureSchema = null;
+  before((done) => {
+    utils.loadSchema(`${__dirname}/pressure.schema.json`).then((parsedSchema) => {
+      pressureSchema = parsedSchema;
+      done();
+    });
+  });
+
+  describe("consume()", () => {
+    it("should decode the Yosensi YO Refrigerant Monitor payload", () => {
+      const data = {
+        data: {
+          port: 0,
+          payloadHex: "0200000008000111a60d00010107100000331600116d00130d00116d0110"  
+        },
+      };
+
+      utils.expectEmits((type, value) => {
+        assert.equal(type, "sample");
+        assert.isNotNull(value);
+        assert.typeOf(value.data, "object");
+
+        assert.equal(value.topic, "batteryVoltage");
+        assert.equal(value.data.batteryVoltage, 4518);
+
+        utils.validateSchema(value.data, batteryVoltageSchema, { throwError: true });
+      });
+
+      utils.expectEmits((type, value) => {
+        assert.equal(type, "sample");
+        assert.isNotNull(value);
+        assert.typeOf(value.data, "object");
+
+        assert.equal(value.topic, "internalTemperature");
+        assert.equal(value.data.internalTemperature, 26.3);
+
+        utils.validateSchema(value.data, internalTemperatureSchema, { throwError: true });
+      });
+
+      utils.expectEmits((type, value) => {
+        assert.equal(type, "sample");
+        assert.isNotNull(value);
+        assert.typeOf(value.data, "object");
+
+        assert.equal(value.topic, "humidity");
+        assert.equal(value.data.humidity, 51);
+
+        utils.validateSchema(value.data, humiditySchema, { throwError: true });
+      });
+
+      utils.expectEmits((type, value) => {
+        assert.equal(type, "sample");
+        assert.isNotNull(value);
+        assert.typeOf(value.data, "object");
+
+        assert.equal(value.topic, "temperature");
+        assert.equal(value.data.temperature, 27.2);
+
+        utils.validateSchema(value.data, temperatureSchema, { throwError: true });
+      });
+
+      utils.expectEmits((type, value) => {
+        assert.equal(type, "sample");
+        assert.isNotNull(value);
+        assert.typeOf(value.data, "object");
+
+        assert.equal(value.topic, "pressure");
+        assert.equal(value.data.pressure, 0.19);
+
+        utils.validateSchema(value.data, pressureSchema, { throwError: true });
+      });
+
+      consume(data);
+    });
+  });
+});
