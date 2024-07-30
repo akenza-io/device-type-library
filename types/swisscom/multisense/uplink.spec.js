45f022c022e7cdc24ae79740e33a44927fd10c27
j2 <jonasg@akenza.com>
2024-07-30 13:57:25 +0200

    Added batteryLevel calculation with dropoff to swisscom multisense

--- types/swisscom/multisense/uplink.spec.js
+++ types/swisscom/multisense/uplink.spec.js
@@ -180,7 +180,7 @@ describe("Swisscom Multisense Uplinks", () => {
         assert.equal(value.data.payloadVersion, 1);
         assert.equal(value.data.mode, 0);
         assert.equal(value.data.batteryVoltage, 3);
-        assert.equal(value.data.batteryLevel, 64);
+        assert.equal(value.data.batteryLevel, 87);
         utils.validateSchema(value.data, lifecycleSchema, { throwError: true });
       });
 
@@ -228,7 +228,7 @@ describe("Swisscom Multisense Uplinks", () => {
         assert.equal(value.data.payloadVersion, 2);
         assert.equal(value.data.mode, 0);
         assert.equal(value.data.batteryVoltage, 3.1);
-        assert.equal(value.data.batteryLevel, 71);
+        assert.equal(value.data.batteryLevel, 100);
         utils.validateSchema(value.data, lifecycleSchema, { throwError: true });
       });
 
@@ -276,7 +276,7 @@ describe("Swisscom Multisense Uplinks", () => {
         assert.equal(value.data.payloadVersion, 2);
         assert.equal(value.data.mode, 0);
         assert.equal(value.data.batteryVoltage, 3);
-        assert.equal(value.data.batteryLevel, 67);
+        assert.equal(value.data.batteryLevel, 100);
         utils.validateSchema(value.data, lifecycleSchema, { throwError: true });
       });
 
