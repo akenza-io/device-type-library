81bb39d37af86b032d64adacfa8f3c1365c067ec
j2 <jonasg@akenza.com>
2024-11-22 10:35:05 +0100

    .

--- types/milesight/EM410-RDL/uplink.js
+++ types/milesight/EM410-RDL/uplink.js
@@ -2,11 +2,6 @@ function readUInt8(bytes) {
   return bytes & 0xff;
 }
 
-function readInt8(bytes) {
-  const ref = readUInt8(bytes);
-  return ref > 0x7f ? ref - 0x100 : ref;
-}
-
 function readUInt16LE(bytes) {
   const value = (bytes[1] << 8) + bytes[0];
   return value & 0xffff;
@@ -21,11 +16,6 @@ function readUInt32LE(bytes) {
   const value =
     (bytes[3] << 24) + (bytes[2] << 16) + (bytes[1] << 8) + bytes[0];
   return (value & 0xffffffff) >>> 0;
-}
-
-function readInt32LE(bytes) {
-  const ref = readUInt32LE(bytes);
-  return ref > 0x7fffffff ? ref - 0x100000000 : ref;
 }
 
 function readProtocolVersion(bytes) {
@@ -60,55 +50,42 @@ function readSerialNumber(bytes) {
   return temp.join("");
 }
 
-function readLoRaWANClass(type) {
-  switch (type) {
-    case 0:
-      return "ClassA";
-    case 1:
-      return "ClassB";
-    case 2:
-      return "ClassC";
-    case 3:
-      return "ClassCtoB";
-  }
-}
-
 function readPositionStatus(status) {
   switch (status) {
     case 0:
-      return "Normal";
+      return "NORMAL";
     case 1:
-      return "Tilt";
+      return "TILT";
     default:
-      return "Unknown";
+      return "UNKNOWN";
   }
 }
 
 function readDistanceAlarm(status) {
   switch (status) {
     case 0:
-      return "Threshold Alarm Release";
+      return "THRESHOLD_ALARM_RELEASE";
     case 1:
-      return "Threshold Alarm";
+      return "THRESHOLD_ALARM";
     case 2:
-      return "Mutation Alarm";
+      return "MUTATION_ALARM";
     default:
-      return "Unknown";
+      return "UNKNOWN";
   }
 }
 
 function readDistanceException(status) {
   switch (status) {
     case 0:
-      return "Blind Spot Alarm Release";
+      return "BLIND_SPOT_ALARM_RELEASE";
     case 1:
-      return "Blind Spot Alarm";
+      return "BLIND_SPOT_ALARM";
     case 2:
-      return "No Target";
+      return "NO_TARGET";
     case 3:
-      return "Sensor Exception";
+      return "SENSOR_EXCEPTION";
     default:
-      return "Unknown";
+      return "UNKNOWN";
   }
 }
 
@@ -116,127 +93,134 @@ function readHistoryEvent(status) {
   const event = [];
 
   if (((status >>> 0) & 0x01) === 0x01) {
-    event.push("Threshold Alarm");
+    event.push("THRESHOLD_ALARM");
   }
   if (((status >>> 1) & 0x01) === 0x01) {
-    event.push("Threshold Alarm Release");
+    event.push("THRESHOLD_ALARM_RELEASE");
   }
   if (((status >>> 2) & 0x01) === 0x01) {
-    event.push("Blind Spot Alarm");
+    event.push("BLIND_SPOT_ALARM");
   }
   if (((status >>> 3) & 0x01) === 0x01) {
-    event.push("Blind Spot Alarm Release");
+    event.push("BLIND_SPOT_ALARM_RELEASE");
   }
   if (((status >>> 4) & 0x01) === 0x01) {
-    event.push("Mutation Alarm");
+    event.push("MUTATION_ALARM");
   }
   if (((status >>> 5) & 0x01) === 0x01) {
-    event.push("Tilt Alarm");
+    event.push("TILT_ALARM");
   }
 
   return event;
 }
 
 // 0xFE
-function handle_downlink_response(channelType, bytes, offset) {
+function handleDownlinkResponse(channelType, bytes, offset) {
   const decoded = {};
 
   switch (channelType) {
-    case 0x06: // distance_alarm
-      var data = readUInt8(bytes[offset]);
-      var min = readInt16LE(bytes.slice(offset + 1, offset + 3));
-      var max = readInt16LE(bytes.slice(offset + 3, offset + 5));
+    case 0x06: {
+      // distance_alarm
+      const data = readUInt8(bytes[offset]);
+      const min = readInt16LE(bytes.slice(offset + 1, offset + 3));
+      const max = readInt16LE(bytes.slice(offset + 3, offset + 5));
       // skip 4 bytes (reserved)
       offset += 9;
 
-      var alarm_type = data & 0x07;
-      var id = (data >>> 3) & 0x07;
-      var alarm_release_report_enable = data >>> 7;
+      const alarmType = data & 0x07;
+      const id = (data >>> 3) & 0x07;
+      const alarmReleaseReportEnable = data >>> 7;
 
-      if (alarm_type === 5 && id === 2) {
-        decoded.distanceMutation_alarm = {};
-        decoded.distanceMutation_alarm.alarm_release_report_enable =
-          alarm_release_report_enable;
-        decoded.distanceMutation_alarm.mutation = max;
+      if (alarmType === 5 && id === 2) {
+        decoded.distanceMutationAlarm = {};
+        decoded.distanceMutationAlarm.alarmReleaseReportEnable =
+          alarmReleaseReportEnable;
+        decoded.distanceMutationAlarm.mutation = max;
       } else {
-        decoded.distance_alarm = {};
-        decoded.distance_alarm.condition = alarm_type;
-        decoded.distance_alarm.alarm_release_report_enable =
-          alarm_release_report_enable;
-        decoded.distance_alarm.min = min;
-        decoded.distance_alarm.max = max;
+        decoded.distanceAlarm = {};
+        decoded.distanceAlarm.condition = alarmType;
+        decoded.distanceAlarm.alarmReleaseReportEnable =
+          alarmReleaseReportEnable;
+        decoded.distanceAlarm.min = min;
+        decoded.distanceAlarm.max = max;
       }
       break;
+    }
     case 0x1b: // distance_range
-      decoded.distance_range = {};
-      decoded.distance_range.mode = readUInt8(bytes[offset]);
+      decoded.distanceRange = {};
+      decoded.distanceRange.mode = readUInt8(bytes[offset]);
       // skip 2 bytes (reserved)
-      decoded.distance_range.max = readUInt16LE(
+      decoded.distanceRange.max = readUInt16LE(
         bytes.slice(offset + 3, offset + 5),
       );
       offset += 5;
       break;
     case 0x1c:
-      decoded.recollection_counts = readUInt8(bytes[offset]);
-      decoded.recollection_interval = readUInt8(bytes[offset + 1]);
+      decoded.recollectionCounts = readUInt8(bytes[offset]);
+      decoded.recollectionInterval = readUInt8(bytes[offset + 1]);
       offset += 2;
       break;
-    case 0x2a: // radar calibration
-      var calibrate_type = readUInt8(bytes[offset]);
+    case 0x2a: {
+      // radar calibration
+      const calibrateType = readUInt8(bytes[offset]);
       offset += 1;
 
-      switch (calibrate_type) {
+      switch (calibrateType) {
         case 0:
-          decoded.radar_calibration = 1;
+          decoded.radarCalibration = 1;
           break;
         case 1:
-          decoded.radar_blind_calibration = 1;
+          decoded.radarBlindCalibration = 1;
           break;
       }
       break;
+    }
     case 0x3e: // tilt_distance_link
-      decoded.tilt_distance_link = readUInt8(bytes[offset]);
+      decoded.tiltDistanceLink = readUInt8(bytes[offset]);
       offset += 1;
       break;
     case 0x4a: // sync_time
-      decoded.sync_time = 1;
+      decoded.syncTime = 1;
       offset += 1;
       break;
     case 0x68: // history_enable
-      decoded.history_enable = readUInt8(bytes[offset]);
+      decoded.historyEnable = readUInt8(bytes[offset]);
       offset += 1;
       break;
     case 0x69: // retransmit_enable
-      decoded.retransmit_enable = readUInt8(bytes[offset]);
+      decoded.retransmitEnable = readUInt8(bytes[offset]);
       offset += 1;
       break;
-    case 0x6a:
-      var interval_type = readUInt8(bytes[offset]);
-      switch (interval_type) {
+    case 0x6a: {
+      const intervalType = readUInt8(bytes[offset]);
+      switch (intervalType) {
         case 0:
-          decoded.retransmit_interval = readUInt16LE(
+          decoded.retransmitInterval = readUInt16LE(
             bytes.slice(offset + 1, offset + 3),
           );
           break;
         case 1:
-          decoded.resend_interval = readUInt16LE(
+          decoded.resendInterval = readUInt16LE(
             bytes.slice(offset + 1, offset + 3),
           );
+          break;
+        default:
           break;
       }
       offset += 3;
       break;
+    }
     case 0x8e: // report_interval
       // ignore the first byte
-      decoded.report_interval = readUInt16LE(
+      decoded.reportInterval = readUInt16LE(
         bytes.slice(offset + 1, offset + 3),
       );
       offset += 3;
       break;
     case 0xab: // distance_calibration
-      decoded.distance_calibration = {};
-      decoded.distance_calibration.enable = readUInt8(bytes[offset]);
-      decoded.distance_calibration.distance = readInt16LE(
+      decoded.distanceCalibration = {};
+      decoded.distanceCalibration.enable = readUInt8(bytes[offset]);
+      decoded.distanceCalibration.distance = readInt16LE(
         bytes.slice(offset + 1, offset + 3),
       );
       offset += 3;
@@ -246,79 +230,93 @@ function handle_downlink_response(channelType, bytes, offset) {
       offset += 2;
       break;
     case 0xf2: // alarm_counts
-      decoded.alarm_counts = readUInt16LE(bytes.slice(offset, offset + 2));
+      decoded.alarmCounts = readUInt16LE(bytes.slice(offset, offset + 2));
       offset += 2;
       break;
     default:
-      throw new Error("unknown downlink response");
+      throw new Error("UNKNOWN_DOWNLINK_RESPONSE");
   }
 
   return { data: decoded, offset };
 }
 
 // 0xF8
-function handle_downlink_response_ext(channelType, bytes, offset) {
+function handleDownlinkResponseExt(channelType, bytes, offset) {
   const decoded = {};
 
   switch (channelType) {
-    case 0x12: // distance_mode
-      var distance_mode_result = readUInt8(bytes[offset + 1]);
-      if (distance_mode_result === 0) {
-        decoded.distance_mode = readUInt8(bytes[offset]);
+    case 0x12: {
+      // distance_mode
+      const distanceModeResult = readUInt8(bytes[offset + 1]);
+      if (distanceModeResult === 0) {
+        decoded.distanceMode = readUInt8(bytes[offset]);
       }
       offset += 2;
       break;
-    case 0x13: // blind_detection_enable
-      var blind_detection_enable_result = readUInt8(bytes[offset + 1]);
-      if (blind_detection_enable_result === 0) {
-        decoded.blind_detection_enable = readUInt8(bytes[offset]);
+    }
+    case 0x13: {
+      // blind_detection_enable
+      const blindDetectionEnableResult = readUInt8(bytes[offset + 1]);
+      if (blindDetectionEnableResult === 0) {
+        decoded.blindDetectionEnable = readUInt8(bytes[offset]);
       }
       offset += 2;
       break;
-    case 0x14: // signal_quality
-      var signal_quality_result = readUInt8(bytes[offset + 2]);
-      if (signal_quality_result === 0) {
-        decoded.signal_quality = readInt16LE(bytes.slice(offset, offset + 2));
+    }
+    case 0x14: {
+      // signal_quality
+      const signalQualityResult = readUInt8(bytes[offset + 2]);
+      if (signalQualityResult === 0) {
+        decoded.signalQuality = readInt16LE(bytes.slice(offset, offset + 2));
       }
       offset += 3;
       break;
-    case 0x15: // distance_threshold_sensitive
-      var distance_threshold_sensitive_result = readUInt8(bytes[offset + 2]);
-      if (distance_threshold_sensitive_result === 0) {
-        decoded.distance_threshold_sensitive =
+    }
+    case 0x15: {
+      // distance_threshold_sensitive
+      const distanceThresholdSensitiveResult = readUInt8(bytes[offset + 2]);
+      if (distanceThresholdSensitiveResult === 0) {
+        decoded.distanceThresholdSensitive =
           readInt16LE(bytes.slice(offset, offset + 2)) / 10;
       }
       offset += 3;
       break;
-    case 0x16: // peak_sorting
-      var peak_sorting_result = readUInt8(bytes[offset + 1]);
-      if (peak_sorting_result === 0) {
-        decoded.peak_sorting = readUInt8(bytes[offset]);
+    }
+    case 0x16: {
+      // peak_sorting
+      const peakSortingResult = readUInt8(bytes[offset + 1]);
+      if (peakSortingResult === 0) {
+        decoded.peakSorting = readUInt8(bytes[offset]);
       }
       offset += 2;
       break;
-    case 0x0d: // retransmit_config
-      var retransmit_config_result = readUInt8(bytes[offset + 3]);
-      if (retransmit_config_result === 0) {
-        decoded.retransmit_config = {};
-        decoded.retransmit_config.enable = readUInt8(bytes[offset]);
-        decoded.retransmit_config.retransmit_interval = readUInt16LE(
+    }
+    case 0x0d: {
+      // retransmit_config
+      const retransmitConfigResult = readUInt8(bytes[offset + 3]);
+      if (retransmitConfigResult === 0) {
+        decoded.retransmitConfig = {};
+        decoded.retransmitConfig.enable = readUInt8(bytes[offset]);
+        decoded.retransmitConfig.retransmitInterval = readUInt16LE(
           bytes.slice(offset + 1, offset + 3),
         );
       }
       offset += 4;
       break;
-    case 0x39: // collection_interval
-      var collection_interval_result = readUInt8(bytes[offset + 2]);
-      if (collection_interval_result === 0) {
-        decoded.collection_interval = readUInt16LE(
+    }
+    case 0x39: {
+      // collection_interval
+      const collectionIntervalResult = readUInt8(bytes[offset + 2]);
+      if (collectionIntervalResult === 0) {
+        decoded.collectionInterval = readUInt16LE(
           bytes.slice(offset, offset + 2),
         );
       }
       offset += 3;
       break;
+    }
     default:
-      throw new Error("unknown downlink response");
+      throw new Error("UNKNOWN_DOWNLINK_RESPONSE");
   }
 
   return { data: decoded, offset };
@@ -332,7 +330,7 @@ if (!Object.assign) {
     value(target) {
       if (target == null) {
         // TypeError if undefined or null
-        throw new TypeError("Cannot convert first argument to object");
+        throw new TypeError("CANNOT_CONVERT_FIRST_ARGUMENT_TO_OBJECT");
       }
 
       const to = Object(target);
@@ -367,6 +365,7 @@ function consume(event) {
   const bytes = Hex.hexToBytes(payload);
   const decoded = {};
   const lifecycle = {};
+  const system = {};
 
   for (let i = 0; i < bytes.length; ) {
     const channelId = bytes[i++];
@@ -374,27 +373,27 @@ function consume(event) {
 
     // DEVICE STATUS
     if (channelId === 0xff && channelType === 0x0b) {
-      decoded.deviceStatus = bytes[i];
+      system.deviceStatus = bytes[i];
       i += 1;
     }
     // IPSO VERSION
     else if (channelId === 0xff && channelType === 0x01) {
-      decoded.ipsoVersion = readProtocolVersion(bytes[i]);
+      system.ipsoVersion = readProtocolVersion(bytes[i]);
       i += 1;
     }
     // SERIAL NUMBER
     else if (channelId === 0xff && channelType === 0x16) {
-      decoded.serialNumber = readSerialNumber(bytes.slice(i, i + 8));
+      system.serialNumber = readSerialNumber(bytes.slice(i, i + 8));
       i += 8;
     }
     // HARDWARE VERSION
     else if (channelId === 0xff && channelType === 0x09) {
-      decoded.hardwareVersion = readHardwareVersion(bytes.slice(i, i + 2));
+      system.hardwareVersion = readHardwareVersion(bytes.slice(i, i + 2));
       i += 2;
     }
     // FIRMWARE VERSION
     else if (channelId === 0xff && channelType === 0x0a) {
-      decoded.firmwareVersion = readFirmwareVersion(bytes.slice(i, i + 2));
+      system.firmwareVersion = readFirmwareVersion(bytes.slice(i, i + 2));
       i += 2;
     }
     // LORAWAN CLASS TYPE
@@ -403,17 +402,17 @@ function consume(event) {
     }
     // TSL VERSION
     else if (channelId === 0xff && channelType === 0xff) {
-      decoded.tslVersion = readTslVersion(bytes.slice(i, i + 2));
+      system.tslVersion = readTslVersion(bytes.slice(i, i + 2));
       i += 2;
     }
     // DEVICE RESET EVENT
     else if (channelId === 0xff && channelType === 0xfe) {
-      decoded.resetEvent = 1;
+      lifecycle.resetEvent = true;
       i += 1;
     }
     // BATTERY
     else if (channelId === 0x01 && channelType === 0x75) {
-      decoded.batteryLevel = readUInt8(bytes[i]);
+      lifecycle.batteryLevel = readUInt8(bytes[i]);
       i += 1;
     }
     // TEMPERATURE
@@ -438,7 +437,7 @@ function consume(event) {
     }
     // DISTANCE ALARM
     else if (channelId === 0x84 && channelType === 0x82) {
-      var data = {};
+      const data = {};
       data.distance = readInt16LE(bytes.slice(i, i + 2));
       data.distanceAlarm = readDistanceAlarm(bytes[i + 2]);
       i += 3;
@@ -449,7 +448,7 @@ function consume(event) {
     }
     // DISTANCE MUTATION ALARM
     else if (channelId === 0x94 && channelType === 0x82) {
-      var data = {};
+      const data = {};
       data.distance = readInt16LE(bytes.slice(i, i + 2));
       data.distanceMutation = readInt16LE(bytes.slice(i + 2, i + 4));
       data.distanceAlarm = readDistanceAlarm(bytes[i + 4]);
@@ -461,12 +460,12 @@ function consume(event) {
     }
     // DISTANCE EXCEPTION ALARM
     else if (channelId === 0xb4 && channelType === 0x82) {
-      var distanceRawData = readUInt16LE(bytes.slice(i, i + 2));
-      var distanceValue = readInt16LE(bytes.slice(i, i + 2));
+      const distanceRawData = readUInt16LE(bytes.slice(i, i + 2));
+      const distanceValue = readInt16LE(bytes.slice(i, i + 2));
       const distanceException = readDistanceException(bytes[i + 2]);
       i += 3;
 
-      var data = {};
+      const data = {};
       if (distanceRawData === 0xfffd || distanceRawData === 0xffff) {
         // IGNORE NO TARGET AND SENSOR EXCEPTION
       } else {
@@ -480,39 +479,39 @@ function consume(event) {
     // HISTORY
     else if (channelId === 0x20 && channelType === 0xce) {
       const timestamp = readUInt32LE(bytes.slice(i, i + 4));
-      var distanceRawData = readUInt16LE(bytes.slice(i + 4, i + 6));
-      var distanceValue = readInt16LE(bytes.slice(i + 4, i + 6));
+      const distanceRawData = readUInt16LE(bytes.slice(i + 4, i + 6));
+      const distanceValue = readInt16LE(bytes.slice(i + 4, i + 6));
       const temperatureRawData = readUInt16LE(bytes.slice(i + 6, i + 8));
       const temperatureValue = readInt16LE(bytes.slice(i + 6, i + 8)) / 10;
       const mutation = readInt16LE(bytes.slice(i + 8, i + 10));
       const eventValue = readUInt8(bytes[i + 10]);
       i += 11;
 
-      var data = {};
+      const data = {};
       data.timestamp = timestamp;
       if (distanceRawData === 0xfffd) {
-        data.distanceException = "No Target";
+        data.distanceException = "NO_TARGET";
       } else if (distanceRawData === 0xffff) {
-        data.distanceException = "Sensor Exception";
+        data.distanceException = "SENSOR_EXCEPTION";
       } else if (distanceRawData === 0xfffe) {
-        data.distanceException = "Disabled";
+        data.distanceException = "DISABLED";
       } else {
         data.distance = distanceValue;
       }
 
       if (temperatureRawData === 0xfffe) {
-        data.temperatureException = "Disabled";
+        data.temperatureException = "DISABLED";
       } else if (temperatureRawData === 0xffff) {
-        data.temperatureException = "Sensor Exception";
+        data.temperatureException = "SENSOR_EXCEPTION";
       } else {
         data.temperature = temperatureValue;
       }
 
-      var event = readHistoryEvent(eventValue);
-      if (event.length > 0) {
-        data.event = event;
+      const historicEvent = readHistoryEvent(eventValue);
+      if (historicEvent.length > 0) {
+        data.historicEvent = historicEvent;
       }
-      if (event.indexOf("Mutation Alarm") !== -1) {
+      if (historicEvent.indexOf("MUTATION_ALARM") !== -1) {
         data.distanceMutation = mutation;
       }
 
@@ -521,14 +520,14 @@ function consume(event) {
     }
     // DOWNLINK RESPONSE
     else if (channelId === 0xfe) {
-      result = handle_downlink_response(channelType, bytes, i);
-      decoded = Object.assign(decoded, result.data);
+      const result = handleDownlinkResponse(channelType, bytes, i);
+      Object.assign(decoded, result.data);
       i = result.offset;
     }
     // DOWNLINK RESPONSE
     else if (channelId === 0xf8) {
-      result = handle_downlink_response_ext(channelType, bytes, i);
-      decoded = Object.assign(decoded, result.data);
+      const result = handleDownlinkResponseExt(channelType, bytes, i);
+      Object.assign(decoded, result.data);
       i = result.offset;
     } else {
       break;
