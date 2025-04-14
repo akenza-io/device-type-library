f7d470b5738a29e9d2ae52f8f6027998e4625b31
lovexiaoxia <aadyx2007@163.com>
2025-04-14 09:19:31 +0800

    Fix

--- types/moko/LW001-BG_PRO/uplink.spec.js
+++ types/moko/LW001-BG_PRO/uplink.spec.js
@@ -6,23 +6,26 @@ describe('Decoder Tests', () => {
         const event = {
             data: {
                 port: 1, // Heartbeat 的 port
-                payloadHex: '010203040506' // 示例 payload
+                payloadHex: '211be0014700000000' // 示例 payload
             }
         };
 
         const result = decoder.consume(event);
 
         expect(result).toEqual({
-            port: 1,
-            hexFormatPayload: "010203040506",
-            payloadType: "Heartbeat",
-            operationMode: "Standby mode",
-            temperature: "3°C",
-            ack: 3,
-            batteryVoltage: "2.5V",
-            rebootReason: "Restart after power failure",
-            firmwareVersion: "V0.0.3",
-            activityCount: 1286
+            "port": 1,
+            "hexFormatPayload": "211be0014700000000",
+            "payloadType": "Heartbeat",
+            "operationMode": "Periodic mode",
+            "tamperAlarm": "Not triggered",
+            "mandownStatus": "Not in idle",
+            "motionStateSinceLastPaylaod": "Yes",
+            "temperature": "27°C",
+            "ack": 0,
+            "batteryVoltage": "3.6V",
+            "rebootReason": "Bluetooth command request",
+            "firmwareVersion": "V1.0.7",
+            "activityCount": 0
         });
     });
 
@@ -31,25 +34,30 @@ describe('Decoder Tests', () => {
         const event = {
             data: {
                 port: 2, // Location Fixed 的 port
-                payloadHex: '02030405060708' // 示例 payload
+                payloadHex: '011be00207e9040b082a33000912278998443aa12912' // 示例 payload
             }
         };
 
         const result = decoder.consume(event);
 
         expect(result).toEqual({
-            port: 2,
-            hexFormatPayload: "02030405060708",
-            payloadType: "Location Fixed",
-            operationMode: "Periodic mode",
-            temperature: "3°C",
-            ack: 3,
-            batteryVoltage: "2.5V",
-            positionSuccessType: "WIFI positioning success",
-            timestamp: "2023-10-15 12:34:56 TZ:8",
-            macData: [
-                { mac: "0x020x030x040x050x060x07", rssi: "-249dBm" }
-            ]
+            "port": 2,
+            "hexFormatPayload": "011be00207e9040b082a33000912278998443aa12912",
+            "payloadType": "Location Fixed",
+            "operationMode": "Periodic mode",
+            "tamperAlarm": "Not triggered",
+            "mandownStatus": "Not in idle",
+            "motionStateSinceLastPaylaod": "No",
+            "positioningType": "Normal",
+            "temperature": "27°C",
+            "ack": 0,
+            "batteryVoltage": "3.6V",
+            "positionSuccessType": "GPS positioning success",
+            "positionSuccessTypeCode": 2,
+            "timestamp": "2025-4-11 8:42:51  TZ:0",
+            "latitude": 30.4581016,
+            "longitude": 114.4693033,
+            "pdop": 1.8
         });
     });
 
@@ -58,26 +66,25 @@ describe('Decoder Tests', () => {
         const event = {
             data: {
                 port: 3, // Location Failure 的 port
-                payloadHex: '03040506070809' // 示例 payload
+                payloadHex: '011af00500' // 示例 payload
             }
         };
 
         const result = decoder.consume(event);
 
         expect(result).toEqual({
-            port: 3,
-            hexFormatPayload: "03040506070809",
-            payloadType: "Location Failure",
-            operationMode: "Timing mode",
-            temperature: "4°C",
-            ack: 4,
-            batteryVoltage: "2.6V",
-            reasonsForPositioningFailure: "WIFI positioning time is not enough (The location payload reporting interval is set too short, please increase the report interval of the current working mode via MKLoRa app)",
-            macData: [
-                { mac: "0x030x040x050x060x070x08", rssi: "-248dBm" }
-            ],
-            pdop: 1.2,
-            gpsSatelliteCn: "1-2-3-4"
+            "port": 3,
+            "hexFormatPayload": "011af00500",
+            "payloadType": "Location Failure",
+            "operationMode": "Periodic mode",
+            "tamperAlarm": "Not triggered",
+            "mandownStatus": "Not in idle",
+            "motionStateSinceLastPaylaod": "No",
+            "positioningType": "Normal",
+            "temperature": "26°C",
+            "ack": 0,
+            "batteryVoltage": "3.7V",
+            "reasonsForPositioningFailure": "Bluetooth broadcasting in progress (Please reduce the Bluetooth broadcast timeout or avoid Bluetooth positioning when Bluetooth broadcasting in process via MKLoRa app)"
         });
     });
 
@@ -86,21 +93,24 @@ describe('Decoder Tests', () => {
         const event = {
             data: {
                 port: 4, // Shutdown 的 port
-                payloadHex: '0405060708090A' // 示例 payload
+                payloadHex: '211af002' // 示例 payload
             }
         };
 
         const result = decoder.consume(event);
 
         expect(result).toEqual({
-            port: 4,
-            hexFormatPayload: "0405060708090A",
-            payloadType: "Shutdown",
-            operationMode: "Motion mode",
-            temperature: "5°C",
-            ack: 5,
-            batteryVoltage: "2.7V",
-            shutdownType: "Bluetooth command to turn off the device"
+            "port": 4,
+            "hexFormatPayload": "211af002",
+            "payloadType": "Shutdown",
+            "operationMode": "Periodic mode",
+            "tamperAlarm": "Not triggered",
+            "mandownStatus": "Not in idle",
+            "motionStateSinceLastPaylaod": "Yes",
+            "temperature": "26°C",
+            "ack": 0,
+            "batteryVoltage": "3.7V",
+            "shutdownType": "Magnetic to turn off the device"
         });
     });
 
@@ -109,21 +119,24 @@ describe('Decoder Tests', () => {
         const event = {
             data: {
                 port: 5, // Vibration 的 port
-                payloadHex: '05060708090A0B' // 示例 payload
+                payloadHex: '211be00001' // 示例 payload
             }
         };
 
         const result = decoder.consume(event);
 
         expect(result).toEqual({
-            port: 5,
-            hexFormatPayload: "05060708090A0B",
-            payloadType: "Shock",
-            operationMode: "Standby mode",
-            temperature: "6°C",
-            ack: 6,
-            batteryVoltage: "2.8V",
-            numberOfShocks: 1234
+            "port": 5,
+            "hexFormatPayload": "211be00001",
+            "payloadType": "Shock",
+            "operationMode": "Periodic mode",
+            "tamperAlarm": "Not triggered",
+            "mandownStatus": "Not in idle",
+            "motionStateSinceLastPaylaod": "Yes",
+            "temperature": "27°C",
+            "ack": 0,
+            "batteryVoltage": "3.6V",
+            "numberOfShocks": 1
         });
     });
 
@@ -132,21 +145,24 @@ describe('Decoder Tests', () => {
         const event = {
             data: {
                 port: 6, // Man Down 的 port
-                payloadHex: '060708090A0B0C' // 示例 payload
+                payloadHex: '1117e10040' // 示例 payload
             }
         };
 
         const result = decoder.consume(event);
 
         expect(result).toEqual({
-            port: 6,
-            hexFormatPayload: "060708090A0B0C",
-            payloadType: "Man Down detection",
-            operationMode: "Periodic mode",
-            temperature: "7°C",
-            ack: 7,
-            batteryVoltage: "2.9V",
-            totalIdleTime: 5678
+            "port": 6,
+            "hexFormatPayload": "1117e10040",
+            "payloadType": "Man Down detection",
+            "operationMode": "Periodic mode",
+            "tamperAlarm": "Not triggered",
+            "mandownStatus": "In idle",
+            "motionStateSinceLastPaylaod": "No",
+            "temperature": "23°C",
+            "ack": 1,
+            "batteryVoltage": "3.6V",
+            "totalIdleTime": 64
         });
     });
 
@@ -155,21 +171,24 @@ describe('Decoder Tests', () => {
         const event = {
             data: {
                 port: 7, // Tamper 的 port
-                payloadHex: '0708090A0B0C0D' // 示例 payload
+                payloadHex: '0b1be007e9040b09073300' // 示例 payload
             }
         };
 
         const result = decoder.consume(event);
 
         expect(result).toEqual({
-            port: 7,
-            hexFormatPayload: "0708090A0B0C0D",
-            payloadType: "Tamper Alarm",
-            operationMode: "Timing mode",
-            temperature: "8°C",
-            ack: 8,
-            batteryVoltage: "3.0V",
-            timestamp: "2023-10-15 12:34:56 TZ:8"
+            "port": 7,
+            "hexFormatPayload": "0b1be007e9040b09073300",
+            "payloadType": "Tamper Alarm",
+            "operationMode": "Motion mode",
+            "tamperAlarm": "Triggered",
+            "mandownStatus": "Not in idle",
+            "motionStateSinceLastPaylaod": "No",
+            "temperature": "27°C",
+            "ack": 0,
+            "batteryVoltage": "3.6V",
+            "timestamp": "2025-4-11 9:7:51  TZ:0"
         });
     });
 
@@ -178,21 +197,24 @@ describe('Decoder Tests', () => {
         const event = {
             data: {
                 port: 8, // Event 的 port
-                payloadHex: '08090A0B0C0D0E' // 示例 payload
+                payloadHex: '231be000' // 示例 payload
             }
         };
 
         const result = decoder.consume(event);
 
         expect(result).toEqual({
-            port: 8,
-            hexFormatPayload: "08090A0B0C0D0E",
-            payloadType: "Event Message",
-            operationMode: "Motion mode",
-            temperature: "9°C",
-            ack: 9,
-            batteryVoltage: "3.1V",
-            eventType: "Start of movement"
+            "port": 8,
+            "hexFormatPayload": "231be000",
+            "payloadType": "Event Message",
+            "operationMode": "Motion mode",
+            "tamperAlarm": "Not triggered",
+            "mandownStatus": "Not in idle",
+            "motionStateSinceLastPaylaod": "Yes",
+            "temperature": "27°C",
+            "ack": 0,
+            "batteryVoltage": "3.6V",
+            "eventType": "Start of movement"
         });
     });
 
@@ -201,25 +223,28 @@ describe('Decoder Tests', () => {
         const event = {
             data: {
                 port: 9, // Battery Consumption 的 port
-                payloadHex: '090A0B0C0D0E0F' // 示例 payload
+                payloadHex: '011ae1000220a60000000000000000000afee500000148' // 示例 payload
             }
         };
 
         const result = decoder.consume(event);
 
         expect(result).toEqual({
-            port: 9,
-            hexFormatPayload: "090A0B0C0D0E0F",
-            payloadType: "Battery Consumption",
-            operationMode: "Standby mode",
-            temperature: "10°C",
-            ack: 10,
-            batteryVoltage: "3.2V",
-            gpsWorkTime: 123456,
-            wifiWorkTime: 234567,
-            bleScanWorkTime: 345678,
-            bleAdvWorkTime: 456789,
-            loraWorkTime: 567890
+            "port": 9,
+            "hexFormatPayload": "011ae1000220a60000000000000000000afee500000148",
+            "payloadType": "Battery Consumption",
+            "operationMode": "Periodic mode",
+            "tamperAlarm": "Not triggered",
+            "mandownStatus": "Not in idle",
+            "motionStateSinceLastPaylaod": "No",
+            "temperature": "26°C",
+            "ack": 1,
+            "batteryVoltage": "3.6V",
+            "gpsWorkTime": 139430,
+            "wifiWorkTime": 0,
+            "bleScanWorkTime": 0,
+            "bleAdvWorkTime": 720613,
+            "loraWorkTime": 328
         });
     });
 
@@ -228,27 +253,27 @@ describe('Decoder Tests', () => {
         const event = {
             data: {
                 port: 12, // Extreme GPS 的 port
-                payloadHex: '0C0D0E0F101112' // 示例 payload
+                payloadHex: '01e112278c97443aa3d411' // 示例 payload
             }
         };
 
         const result = decoder.consume(event);
 
         expect(result).toEqual({
-            port: 12,
-            hexFormatPayload: "0C0D0E0F101112",
-            payloadType: "GPS Limit",
-            operationMode: "Periodic mode",
-            batteryLevel: "Normal",
-            tamperAlarm: "Not triggered",
-            mandownStatus: "Not in idle",
-            motionStateSinceLastPaylaod: "No",
-            positioningType: "Normal",
-            lorawanDownlinkCount: 3,
-            batteryVoltage: "3.3V",
-            latitude: 12.3456789,
-            longitude: 98.7654321,
-            pdop: 1.5
+            "port": 12,
+            "hexFormatPayload": "01e112278c97443aa3d411",
+            "payloadType": "GPS Limit",
+            "operationMode": "Periodic mode",
+            "batteryLevel": "Normal",
+            "tamperAlarm": "Not triggered",
+            "mandownStatus": "Not in idle",
+            "motionStateSinceLastPaylaod": "No",
+            "positioningType": "Normal",
+            "lorawanDownlinkCount": 1,
+            "batteryVoltage": "3.6V",
+            "latitude": 30.4581783,
+            "longitude": 114.4693716,
+            "pdop": 1.7
         });
     });
 });
