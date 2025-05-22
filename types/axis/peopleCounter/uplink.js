1c73e7b636918986562bfdaeebe8f55aab10560f
j2 <jonasg@akenza.com>
2025-05-22 11:46:20 +0200

    Added axis people counter

---
+++ types/axis/peopleCounter/uplink.js
@@ -0,0 +1,20 @@
+function consume(event) {
+  const { measurements } = event.data.data;
+
+  measurements.forEach((measurement) => {
+    const sample = { fw: 0, bw: 0 };
+    const timestamp = new Date(measurement.utcTo);
+
+    measurement.items.forEach((item) => {
+      if (item.direction === "in") {
+        sample.fw = item.count;
+      } else if (item.direction === "out") {
+        sample.bw = item.count;
+      }
+    });
+
+    if (sample.fw !== 0 || sample.bw !== 0) {
+      emit("sample", { data: sample, topic: "line_count", timestamp });
+    }
+  });
+}
