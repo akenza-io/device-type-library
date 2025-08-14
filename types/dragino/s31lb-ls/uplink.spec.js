cc996b112216150b028f3cdcc63ed27e9e524b84
t1lloAK <tillo@akenza.io>
2025-08-14 17:11:23 +0200

    testing shell

--- types/dragino/s31lb-ls/uplink.spec.js
+++ types/dragino/s31lb-ls/uplink.spec.js
@@ -1,1 +1,18 @@
-// test the uplink.js 
+// test script for uplink.js decoder
+const chai = require("chai");
+const rewire = require("rewire");
+const utils = require("test-utils");
+const { assert } = chai;
+
+describe("Dragino NSE01 Uplink", () => {  // Group of related tests
+    // Setup code runs before tests
+    before((done) => {
+        // Load schemas and prepare test environment
+    });
+
+    describe("consume()", () => {           // Test specific function
+        it("should decode the Dragino NSE01 report uplink", () => {
+            // Individual test case
+        });
+    });
+});
