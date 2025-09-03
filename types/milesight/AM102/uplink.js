ff3117a54a61b17686ec49351abd4f37ad88a038
t1lloAK <tillo@akenza.io>
2025-09-03 15:26:37 +0200

    fixed lint

--- types/milesight/AM102/uplink.js
+++ types/milesight/AM102/uplink.js
@@ -14,65 +14,64 @@ function readUInt32LE(bytes) {
 }
 
 function readSerialNumber(bytes) {
-  let temp = [];
+  const temp = [];
   for (let idx = 0; idx < bytes.length; idx++) {
-    temp.push(("0" + (bytes[idx] & 0xff).toString(16)).slice(-2));
+    temp.push((`0${(bytes[idx] & 0xff).toString(16)}`).slice(-2));
   }
   return temp.join("");
 }
 
 function consume(event) {
   const bytes = Hex.hexToBytes(event.data.payloadHex);
-  const decoded = {};
   const climateData = {};
   const lifecycleData = {};
   const systemData = {};
 
   for (let i = 0; i < bytes.length;) {
-    const channel_id = bytes[i++];
-    const channel_type = bytes[i++];
+    const channelId = bytes[i++];
+    const channelType = bytes[i++];
 
     // HARDWARE VERSION
-    if (channel_id === 0xff && channel_type === 0x09) {
+    if (channelId === 0xff && channelType === 0x09) {
       const major = bytes[i];
       const minor = bytes[i + 1] >> 4;
-      systemData.hardware_version = "v" + major + "." + minor;
+      systemData.hardware_version = `v${major}.${minor}`;
       i += 2;
     }
     // FIRMWARE VERSION
-    else if (channel_id === 0xff && channel_type === 0x0a) {
+    else if (channelId === 0xff && channelType === 0x0a) {
       const major = bytes[i];
       const minor = bytes[i + 1];
-      systemData.firmware_version = "v" + major + "." + minor;
+      systemData.firmware_version = `v${major}.${minor}`;
       i += 2;
     }
     // SERIAL NUMBER
-    else if (channel_id === 0xff && channel_type === 0x16) {
+    else if (channelId === 0xff && channelType === 0x16) {
       systemData.sn = readSerialNumber(bytes.slice(i, i + 8));
       i += 8;
     }
     // DEVICE STATUS
-    else if (channel_id === 0xff && channel_type === 0x0b) {
+    else if (channelId === 0xff && channelType === 0x0b) {
       systemData.device_status = "on"; // Presence of this channel indicates 'on'
       i += 1;
     }
     // BATTERY
-    else if (channel_id === 0x01 && channel_type === 0x75) {
+    else if (channelId === 0x01 && channelType === 0x75) {
       lifecycleData.battery = bytes[i];
       i += 1;
     }
     // TEMPERATURE
-    else if (channel_id === 0x03 && channel_type === 0x67) {
+    else if (channelId === 0x03 && channelType === 0x67) {
       climateData.temperature = readInt16LE(bytes.slice(i, i + 2)) / 10;
       i += 2;
     }
     // HUMIDITY
-    else if (channel_id === 0x04 && channel_type === 0x68) {
+    else if (channelId === 0x04 && channelType === 0x68) {
       climateData.humidity = bytes[i] / 2;
       i += 1;
     }
     // HISTORY DATA
-    else if (channel_id === 0x20 && channel_type === 0xce) {
+    else if (channelId === 0x20 && channelType === 0xce) {
       const ts = readUInt32LE(bytes.slice(i, i + 4));
       const temp = readInt16LE(bytes.slice(i + 4, i + 6)) / 10;
       const hum = bytes[i + 6] / 2;
@@ -83,17 +82,13 @@ function consume(event) {
       };
       emit("sample", {
         data: historicalData,
-        topic: "climate",
+        topic: "default",
         timestamp: new Date(ts * 1000),
       });
 
       i += 7;
     } else {
-      // To prevent infinite loops on malformed payloads, we skip the rest of the payload
-      emit("log", {
-        level: "warning",
-        message: `Unknown channel/type: ${channel_id}/${channel_type}`,
-      });
+
       break;
     }
   }
@@ -101,7 +96,7 @@ function consume(event) {
   if (Object.keys(climateData).length > 0) {
     emit("sample", {
       data: climateData,
-      topic: "climate"
+      topic: "default"
     });
   }
   if (Object.keys(lifecycleData).length > 0) {
