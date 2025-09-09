e7f259da4be34ea2b2bd5db9a31955e0d8fa12d8
t1lloAK <tillo@akenza.io>
2025-09-09 16:24:22 +0200

    updated voc to vocPercentage

--- types/thermokon/mcs-lrw/uplink.js
+++ types/thermokon/mcs-lrw/uplink.js
@@ -259,7 +259,7 @@ function consume(event) {
     data.co2 = decoded.CO2;
   }
   if (decoded.VOC !== undefined) {
-    data.volatileOrganicCompounds = decoded.VOC;
+    data.vocPercentage = decoded.VOC;
   }
   if (decoded.ATM_P !== undefined) {
     data.pressure = decoded.ATM_P;
