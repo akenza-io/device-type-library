e3281b442c2945cf11b936e08e55a8be0d487cba
Alexander Schäpper <aschaepper@gmail.com>
2026-01-28 16:49:41 +0100

    Added a realworld example of a iolink payload and added a test to it.

--- types/hilscher/NFS-M8-QM-D2-N16/uplink.spec.js
+++ types/hilscher/NFS-M8-QM-D2-N16/uplink.spec.js
@@ -25,9 +25,7 @@ describe("NFS-M8-QM-D2-N16 Uplink", () => {
 
   describe("consume()", () => {
     it("should decode the hilscher system monitoring payload", () => {
-      const data = { data: { data: [{ "cpu": { "user": 27.8, "system": 8.7, "uptime": 425618, "average": 38 }, "memory": { "used": 1165074432, "cache": 400633856, "free": 463405056, "available": 690655232, "total": 2052063232, "buffers": 22949888 }, "network": { "received": 27985, "sent": 19661 }, "process": { "all": 0, "running": 0, "blocked": 0, "sleeping": 0, "idle": 0, "unknown": 0, "processDetails": [] } }] } };
-
-
+      const system_monitoring_data = { data: { data: [{ "cpu": { "user": 27.8, "system": 8.7, "uptime": 425618, "average": 38 }, "memory": { "used": 1165074432, "cache": 400633856, "free": 463405056, "available": 690655232, "total": 2052063232, "buffers": 22949888 }, "network": { "received": 27985, "sent": 19661 }, "process": { "all": 0, "running": 0, "blocked": 0, "sleeping": 0, "idle": 0, "unknown": 0, "processDetails": [] } }] } };
 
       expectEmits((type, value) => {
         assert.typeOf(value.data, "object");
@@ -40,7 +38,12 @@ describe("NFS-M8-QM-D2-N16 Uplink", () => {
         validateSchema(value.data, system_monitoring_schema, { throwError: true });
       });
 
-      consume(data);
+      consume(system_monitoring_data);
+
+      const variable_IOLink_data = { data: { Messages: [{"DataSetWriterId":25,"SequenceNumber":11815,"Timestamp":"2026-01-28T15:45:41.014Z","Filter":"processDataInput","Source":"vega.com/VEGAPOINT/VEGAPOINT,2021/52037373","Payload":{"CustomKey_Parent":"192.168.0.2:4840","CustomKey_Port":"Port X2","CustomKey_UserTag":"","Measured value":90.4,"Switching output 1":true,"Switching output 2":false,"Temperature":25.6}}] } };
+
+      consume(variable_IOLink_data);
+
     });
   });
 });
