a914791ca590ecb1325e0634c8ec6c73c347ea52
j2 <jonasg@akenza.com>
2026-01-29 14:06:43 +0100

    Fixed test and camelCased all datakeys

--- types/hilscher/NFS-M8-QM-D2-N16/uplink.js
+++ types/hilscher/NFS-M8-QM-D2-N16/uplink.js
@@ -1,8 +1,11 @@
-/**
-* The consume(event) function is the entry point for the script and will be invoked upon execution.
-* An error will be returned if the script doesn't implement a consume(event) function.
-* @param {ConsumeEvent} event
-*/
+function toCamelCase(str) {
+    return str.replace(/(_([a-z0-9]))|( ([a-z0-9]))/g, (match) => {
+        let transformed = match.replace(/[^0-9a-z ]/gi, '');
+        return transformed.toUpperCase();
+    });
+}
+
+
 function consume(event) {
 
     if (event.data.data != null) {
@@ -20,13 +23,16 @@ function consume(event) {
                 const key = message.Source;
                 const uriComponent = key.replace(/,/g, '%');
                 const dnpDecoded = decodeURIComponent(uriComponent);
-                const topic = dnpDecoded.replace(/[^a-zA-Z0-9]+/g, '_').replace(/^_+|_+$/g, '') + "-" + message.Filter
+                const topic = dnpDecoded.replace(/[^a-zA-Z0-9]+/g, '_').replace(/^_+|_+$/g, '') + "-" + message.Filter;
 
                 /* sanitize payload keys */
                 const newPayload = {};
                 const payload = message.Payload;
                 Object.keys(payload).forEach(key => {
-                    const newKey = key.replace(/[^a-zA-Z0-9]+/g, '_').replace(/^_+|_+$/g, '').replace(/ /g, "_");
+                    let newKey = key[0].toLowerCase() + key.substring(1); // Lowercases starting character
+                    newKey = toCamelCase(newKey); // Make it camelcase
+                    newKey = newKey.replace(/[^0-9a-z]/gi, ''); // Removes non alphanumeric chars
+
                     newPayload[newKey] = payload[key];
                 });
 
