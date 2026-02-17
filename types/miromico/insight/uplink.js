30e1ca8c3e0779b3db28311435017d20e8876ace
t1lloAK <tillo@akenza.io>
2026-02-17 15:21:41 +0100

    updated decoder

--- types/miromico/insight/uplink.js
+++ types/miromico/insight/uplink.js
@@ -1,115 +1,326 @@
+/**
+ * Copyright (c) akenza.io
+ *
+ * @summary Payload Decoder for miromico - insight
+ * @author akenza.io - dta
+ *
+ * Created at : 2026-02-16
+ */
+
+const DSTYPES = {
+  DS_TEMP_HUM: 0x01,
+  DS_CO2: 0x02,
+  DS_COMMON_SETTINGS: 0x05,
+  DS_CO2_SETTINGS: 0x06,
+  DS_BATTERY_VOLTAGE: 0x09,
+  DS_FW_HASH: 0x0a,
+  DS_DOOR_ALARM: 0x0b,
+  DS_DOOR_ALARM_CLEARED: 0x0c,
+  DS_DOOR_STATUS: 0x0d,
+  DS_DOOR_SETTINGS: 0x0e,
+  DS_IAQ_DATA: 0x0f,
+  DS_PRESSURE_DATA: 0x10,
+  DS_REPORT_INTERVAL: 0x11,
+  DS_LIGHT_INTENSITY: 0x14,
+  DS_CONDITIONAL_TX: 0x15,
+  DS_BLIND_ADR: 0x16,
+  DS_LIGHT_MEASUREMENT: 0x17,
+  DS_PARTICULATE_MATTER: 0x18,
+  DS_NOISE_ALARM: 0x19,
+  DS_MICROPHONE_SETTINGS: 0x1a,
+  DS_PM_SETTINGS: 0x1b,
+};
+
+function readUInt16LE(bytes, offset) {
+  return (bytes[offset] | (bytes[offset + 1] << 8)) >>> 0;
+}
+
+function readInt16LE(bytes, offset) {
+  let val = readUInt16LE(bytes, offset);
+  if (val & 0x8000) {
+    val = val - 0x10000;
+  }
+  return val;
+}
+
+function readUInt24LE(bytes, offset) {
+  return (
+    (bytes[offset] | (bytes[offset + 1] << 8) | (bytes[offset + 2] << 16)) >>>
+    0
+  );
+}
+
+function readUInt32LE(bytes, offset) {
+  return (
+    ((bytes[offset + 3] << 24) >>> 0) +
+    (bytes[offset + 2] << 16) +
+    (bytes[offset + 1] << 8) +
+    bytes[offset]
+  );
+}
+
 function consume(event) {
-  const payload = event.data.payloadHex;
-  const bits = Bits.hexToBits(payload);
-  const lifecycle = {};
-  const settings = {};
-  const temperature = {};
-  const co2 = {};
+  const bytes = Hex.hexToBytes(event.data.payloadHex);
+  const { port } = event.data;
 
-  for (let pointer = 0; pointer < bits.length; ) {
-    let measurement = "";
-    let length = (Bits.bitsToUnsigned(bits.substr(pointer, 8)) - 1) * 8;
-    const msgtype = Bits.bitsToUnsigned(bits.substr((pointer += 8), 8));
-    pointer += 8;
-    length += pointer;
+  if (port !== 15) {
+    return;
+  }
 
-    switch (msgtype) {
-      case 1:
-        while (pointer < length) {
-          temperature[`temperature${measurement}`] =
-            Hex.hexLittleEndianToBigEndian(
-              payload.substr(pointer / 4, 4),
-              true,
-            ) * 0.01;
-          pointer += 16;
-          temperature[`humidity${measurement}`] =
-            Hex.hexLittleEndianToBigEndian(
-              payload.substr(pointer / 4, 2),
-              true,
-            ) * 0.5;
-          pointer += 8;
-          if (measurement === "") {
-            measurement = 2;
+  const rawData = {
+    tempHum: [],
+    co2: [],
+    pressure: [],
+    iaq: [],
+    pm: [],
+    light: null,
+    doorEvents: [],
+    configs: {},
+    lifecycle: {},
+  };
+
+  let i = 0;
+  let measurementInterval = -1;
+
+  while (i < bytes.length) {
+    const length = bytes[i++];
+    const type = bytes[i++];
+
+    if (i + length - 1 > bytes.length) {
+      break;
+    }
+
+    const chunk = bytes.slice(i, i + length - 1);
+
+    switch (type) {
+      case DSTYPES.DS_TEMP_HUM: {
+        const nMeas = (length - 1) / 3;
+        for (let j = 0; j < nMeas; j++) {
+          const tRaw = readInt16LE(chunk, j * 3);
+          const hRaw = chunk[j * 3 + 2];
+          const entry = {};
+          if (tRaw !== -32768 || hRaw !== 255) {
+            entry.temperature = tRaw / 100;
+            entry.humidity = hRaw / 2;
+            rawData.tempHum.push(entry);
           } else {
-            measurement++;
+            rawData.tempHum.push(null);
           }
         }
         break;
-      case 2:
-        while (pointer < length) {
-          const co2msb = bits.substr(pointer, 8);
-          pointer += 8;
-          const co2lsb = bits.substr(pointer, 8);
-          pointer += 8;
-          co2[`co2${measurement}`] = Bits.bitsToUnsigned(co2lsb + co2msb);
-
-          if (measurement === "") {
-            measurement = 1;
+      }
+      case DSTYPES.DS_CO2: {
+        const nMeas = (length - 1) / 2;
+        for (let j = 0; j < nMeas; j++) {
+          const cRaw = readUInt16LE(chunk, j * 2);
+          rawData.co2.push({ co2: cRaw });
+        }
+        break;
+      }
+      case DSTYPES.DS_COMMON_SETTINGS: {
+        const interval = readUInt16LE(chunk, 0);
+        const flags = chunk[3];
+        rawData.configs.measurementInterval = interval;
+        rawData.configs.sendCycle = chunk[2];
+        rawData.configs.confirmedUplinks = !!(flags & 0x80);
+        rawData.configs.ledOn = !!(flags & 0x40);
+        rawData.configs.adrOn = !!(flags & 0x20);
+        rawData.configs.contVocMeas = !!(flags & 0x10);
+        rawData.configs.reportInterval = !!(flags & 0x08);
+        if (length === 6) {
+          rawData.configs.retransmissions = chunk[4] & 0x0f;
+        } else {
+          rawData.configs.retransmissions = flags & 0x0f;
+        }
+        if (measurementInterval === -1) {
+          measurementInterval = interval;
+        }
+        break;
+      }
+      case DSTYPES.DS_CO2_SETTINGS: {
+        rawData.configs.co2Subsamples = readUInt16LE(chunk, 2);
+        rawData.configs.abcPeriod = readUInt16LE(chunk, 4);
+        break;
+      }
+      case DSTYPES.DS_BATTERY_VOLTAGE: {
+        rawData.lifecycle.batteryVoltage = readUInt16LE(chunk, 0) / 100;
+        break;
+      }
+      case DSTYPES.DS_FW_HASH: {
+        rawData.lifecycle.firmwareHash = readUInt32LE(chunk, 0).toString(16);
+        break;
+      }
+      case DSTYPES.DS_DOOR_ALARM: {
+        rawData.doorEvents.push({
+          open: true,
+          doorOpenCounter: readUInt32LE(chunk, 0),
+          alarmCounter: readUInt16LE(chunk, 4),
+          alarmTime: readUInt16LE(chunk, 6),
+        });
+        break;
+      }
+      case DSTYPES.DS_DOOR_ALARM_CLEARED: {
+        rawData.doorEvents.push({
+          open: false,
+          doorOpenCounter: readUInt32LE(chunk, 0),
+          alarmCounter: readUInt16LE(chunk, 4),
+        });
+        break;
+      }
+      case DSTYPES.DS_DOOR_STATUS: {
+        rawData.doorEvents.push({
+          open: chunk[6] !== 0,
+          doorOpenCounter: readUInt32LE(chunk, 0),
+          alarmCounter: readUInt16LE(chunk, 4),
+        });
+        break;
+      }
+      case DSTYPES.DS_DOOR_SETTINGS: {
+        rawData.configs.alarmTime = readUInt16LE(chunk, 0);
+        rawData.configs.hallDebounce = readUInt16LE(chunk, 2);
+        rawData.configs.doorStatusTime = readUInt32LE(chunk, 4);
+        break;
+      }
+      case DSTYPES.DS_IAQ_DATA: {
+        const nMeas = (length - 1) / 2;
+        for (let j = 0; j < nMeas; j++) {
+          const iaqRaw = readUInt16LE(chunk, j * 2);
+          if (iaqRaw !== 0xffff) {
+            rawData.iaq.push({
+              iaqIndex: iaqRaw & 0x3fff,
+              iaqAccuracy: iaqRaw >> 14,
+            });
           } else {
-            measurement++;
+            rawData.iaq.push(null);
           }
         }
         break;
-      case 3:
-        lifecycle.consumption = Hex.hexLittleEndianToBigEndian(
-          payload.substr(pointer / 4, 6),
-          false,
-        );
-        pointer += 32;
+      }
+      case DSTYPES.DS_PRESSURE_DATA: {
+        const nMeas = (length - 1) / 3;
+        for (let j = 0; j < nMeas; j++) {
+          rawData.pressure.push({ pressure: readUInt24LE(chunk, j * 3) });
+        }
         break;
-      case 5:
-        settings.measurementInterval = Hex.hexLittleEndianToBigEndian(
-          payload.substr(pointer / 4, 4),
-          false,
-        );
-        pointer += 16;
-        settings.temperatureSamples = Hex.hexLittleEndianToBigEndian(
-          payload.substr(pointer / 4, 2),
-          false,
-        );
-        pointer += 16;
+      }
+      case DSTYPES.DS_REPORT_INTERVAL: {
+        measurementInterval = readUInt16LE(chunk, 0);
         break;
-      case 6:
-        pointer += 16;
-        settings.co2Subsample = Hex.hexLittleEndianToBigEndian(
-          payload.substr(pointer / 4, 4),
-          false,
-        );
-        pointer += 16;
-        settings.abcCalibrationPeriod = Hex.hexLittleEndianToBigEndian(
-          payload.substr(pointer / 4, 4),
-          false,
-        );
-        pointer += 16;
+      }
+      case DSTYPES.DS_LIGHT_INTENSITY: {
+        rawData.light = readUInt16LE(chunk, 0);
         break;
-      case 10:
-        lifecycle.batteryVoltage =
-          (Bits.bitsToUnsigned(bits.substr(pointer, 8)) + 170) / 100;
-        pointer += 8;
+      }
+      case DSTYPES.DS_CONDITIONAL_TX: {
+        rawData.configs.co2Threshold = readUInt16LE(chunk, 0);
+        rawData.configs.tempThreshold = readInt16LE(chunk, 2);
+        rawData.configs.humThreshold = readUInt16LE(chunk, 4);
         break;
-      case 11:
-        settings.firmwareHash = payload.substr(pointer / 4, 8);
-        pointer += 32;
+      }
+      case DSTYPES.DS_BLIND_ADR: {
+        rawData.configs.blindAdrProfile = chunk[0];
         break;
+      }
+      case DSTYPES.DS_LIGHT_MEASUREMENT: {
+        rawData.configs.lightInterval = chunk[0];
+        break;
+      }
+      case DSTYPES.DS_PARTICULATE_MATTER: {
+        const nMeas = (length - 1) / 7;
+        for (let j = 0; j < nMeas; j++) {
+          const offset = j * 7;
+          const pm2_5Raw = readUInt16LE(chunk, offset);
+          const pm1Raw = readUInt16LE(chunk, offset + 2);
+          const pm10Raw = readUInt16LE(chunk, offset + 4);
+          const obstructed = chunk[offset + 6] !== 0;
+          if (pm2_5Raw === 0 && pm1Raw === 0 && pm10Raw === 0 && !obstructed) {
+            rawData.pm.push(null);
+          } else {
+            rawData.pm.push({
+              pm2_5: pm2_5Raw / 10,
+              pm1: pm1Raw / 10,
+              pm10: pm10Raw / 10,
+              pmObstructed: obstructed,
+            });
+          }
+        }
+        break;
+      }
+      case DSTYPES.DS_NOISE_ALARM: {
+        rawData.lifecycle.noiseAlarm = true;
+        rawData.lifecycle.noiseLevel = chunk[0];
+        break;
+      }
+      case DSTYPES.DS_MICROPHONE_SETTINGS: {
+        rawData.configs.noiseHoldIntervalMin = chunk[0];
+        rawData.configs.noiseAbsThreshold = readUInt16LE(chunk, 1);
+        break;
+      }
+      case DSTYPES.DS_PM_SETTINGS: {
+        rawData.configs.pmIntegrationTimeS = chunk[0];
+        break;
+      }
       default:
-        lifecycle.error = "Wrong length of payload";
         break;
+    }
+
+    i += length - 1;
+  }
+
+  // EMIT LOGIC
+
+  const now = new Date();
+  const mergedSamples = {};
+
+  function addSample(time, data) {
+    const tsKey = time.toISOString();
+    if (!mergedSamples[tsKey]) {
+      mergedSamples[tsKey] = { timestamp: time, data: {} };
+    }
+    Object.assign(mergedSamples[tsKey].data, data);
+  }
+
+  function processHistory(arr, dataExtractor) {
+    const len = arr.length;
+    for (let k = 0; k < len; k++) {
+      if (arr[k] === null) continue;
+      let offsetSec = 0;
+      if (measurementInterval > 0) {
+        offsetSec = (len - 1 - k) * measurementInterval;
+      }
+      const time = new Date(now.getTime() - offsetSec * 1000);
+      addSample(time, dataExtractor(arr[k]));
     }
   }
 
-  if (Object.keys(lifecycle).length > 0) {
-    emit("sample", { data: lifecycle, topic: "lifecycle" });
+  processHistory(rawData.tempHum, (item) => item);
+  processHistory(rawData.co2, (item) => item);
+  processHistory(rawData.pressure, (item) => item);
+  processHistory(rawData.iaq, (item) => item);
+  processHistory(rawData.pm, (item) => item);
+
+  if (rawData.light !== null) {
+    addSample(now, { light: rawData.light });
   }
 
-  if (Object.keys(settings).length > 0) {
-    emit("sample", { data: settings, topic: "settings" });
+  // Emit merged default samples
+  const timestamps = Object.keys(mergedSamples).sort();
+  for (const ts of timestamps) {
+    emit("sample", { data: mergedSamples[ts].data, topic: "default" });
   }
 
-  if (Object.keys(temperature).length > 0) {
-    emit("sample", { data: temperature, topic: "temperature" });
+  // Emit Lifecycle
+  if (Object.keys(rawData.lifecycle).length > 0) {
+    emit("sample", { data: rawData.lifecycle, topic: "lifecycle" });
   }
 
-  if (Object.keys(co2).length > 0) {
-    emit("sample", { data: co2, topic: "co2" });
+  // Emit Door Events
+  for (const doorEvt of rawData.doorEvents) {
+    emit("sample", { data: doorEvt, topic: "door" });
+  }
+
+  // Emit Configuration
+  if (Object.keys(rawData.configs).length > 0) {
+    emit("sample", { data: rawData.configs, topic: "configuration" });
   }
 }
--- types/miromico/insight/uplink.spec.js
+++ types/miromico/insight/uplink.spec.js
@@ -1,118 +1,255 @@
-
-
 import { assert } from "chai";
 import rewire from "rewire";
 import { init, loadSchema, expectEmits, validateSchema } from "test-utils";
 
-import { dirname } from 'path';
-import { fileURLToPath } from 'url';
+import { dirname } from "path";
+import { fileURLToPath } from "url";
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = dirname(__filename);
 
-describe("Miromico insight Uplink", () => {
-  let co2Schema = null;
+describe("Miromico insight enhanced Uplink", () => {
+  let defaultSchema = null;
+  let lifecycleSchema = null;
   let consume = null;
-  before((done) => {
+
+  before(async () => {
     const script = rewire(`${__dirname}/uplink.js`);
     consume = init(script);
-    loadSchema(`${__dirname}/co2.schema.json`).then((parsedSchema) => {
-      co2Schema = parsedSchema;
-      done();
-    });
-  });
 
-  let lifecycleSchema = null;
-  before((done) => {
-    loadSchema(`${__dirname}/lifecycle.schema.json`)
-      .then((parsedSchema) => {
-        lifecycleSchema = parsedSchema;
-        done();
-      });
-  });
-
-  let settingsSchema = null;
-  before((done) => {
-    loadSchema(`${__dirname}/settings.schema.json`)
-      .then((parsedSchema) => {
-        settingsSchema = parsedSchema;
-        done();
-      });
-  });
-
-  let temperatureSchema = null;
-  before((done) => {
-    loadSchema(`${__dirname}/temperature.schema.json`)
-      .then((parsedSchema) => {
-        temperatureSchema = parsedSchema;
-        done();
-      });
+    [defaultSchema, lifecycleSchema] = await Promise.all([
+      loadSchema(`${__dirname}/default.schema.json`),
+      loadSchema(`${__dirname}/lifecycle.schema.json`),
+    ]);
   });
 
   describe("consume()", () => {
-    it("should decode Miromico insight payload", () => {
+    it("should decode payload 1 with zero PM values", () => {
       const data = {
         data: {
-          port: 1,
+          port: 15,
           payloadHex:
-            "0701C40978F809770505B00404C40706100020008001050310000000020ABE050B0389A2B903020b03",
+            "0401f208410302da01030f32400314330008180000000000000003096201",
         },
       };
 
       expectEmits((type, value) => {
         assert.equal(type, "sample");
-        assert.isNotNull(value);
-        assert.typeOf(value.data, "object");
+        assert.equal(value.topic, "default");
+        assert.isObject(value.data);
+        assert.equal(value.data.temperature, 22.9);
+        assert.equal(value.data.humidity, 32.5);
+        assert.equal(value.data.co2, 474);
+        assert.equal(value.data.iaqIndex, 50);
+        assert.equal(value.data.iaqAccuracy, 1);
+        assert.equal(value.data.light, 51);
+        assert.notProperty(value.data, "pm2_5");
+        assert.notProperty(value.data, "pm1");
+        assert.notProperty(value.data, "pm10");
+        assert.notProperty(value.data, "pmObstructed");
+        validateSchema(value.data, defaultSchema, { throwError: true });
+      });
 
+      expectEmits((type, value) => {
+        assert.equal(type, "sample");
         assert.equal(value.topic, "lifecycle");
-        assert.equal(value.data.consumption, 16);
-        assert.equal(value.data.batteryVoltage, 3.6);
-
+        assert.isObject(value.data);
+        assert.equal(value.data.batteryVoltage, 3.54);
+        assert.equal(value.data.batteryLevel, 85);
         validateSchema(value.data, lifecycleSchema, { throwError: true });
       });
 
+      consume(data);
+    });
+
+    it("should decode payload 2 with different CO2 and light values", () => {
+      const data = {
+        data: {
+          port: 15,
+          payloadHex:
+            "0401fa084103020902030f32400314290008180000000000000003096201",
+        },
+      };
+
       expectEmits((type, value) => {
         assert.equal(type, "sample");
-        assert.isNotNull(value);
-        assert.typeOf(value.data, "object");
-
-        assert.equal(value.topic, "settings");
-        assert.equal(value.data.measurementInterval, 1200);
-        assert.equal(value.data.temperatureSamples, 4);
-        assert.equal(value.data.co2Subsample, 32);
-        assert.equal(value.data.abcCalibrationPeriod, 384);
-        assert.equal(value.data.firmwareHash, "0389A2B9");
-
-        validateSchema(value.data, settingsSchema, { throwError: true });
+        assert.equal(value.topic, "default");
+        assert.isObject(value.data);
+        assert.equal(value.data.temperature, 22.98);
+        assert.equal(value.data.humidity, 32.5);
+        assert.equal(value.data.co2, 521);
+        assert.equal(value.data.iaqIndex, 50);
+        assert.equal(value.data.iaqAccuracy, 1);
+        assert.equal(value.data.light, 41);
+        assert.notProperty(value.data, "pm2_5");
+        validateSchema(value.data, defaultSchema, { throwError: true });
       });
 
       expectEmits((type, value) => {
         assert.equal(type, "sample");
-        assert.isNotNull(value);
-        assert.typeOf(value.data, "object");
-
-        assert.equal(value.topic, "temperature");
-        assert.equal(value.data.temperature, 25);
-        assert.equal(value.data.humidity, 60);
-        assert.equal(value.data.temperature2, 25.52);
-        assert.equal(value.data.humidity2, 59.5);
-
-        validateSchema(value.data, temperatureSchema, {
-          throwError: true,
-        });
-      });
-
-      expectEmits((type, value) => {
-        assert.equal(type, "sample");
-        assert.isNotNull(value);
-        assert.typeOf(value.data, "object");
-
-        assert.equal(value.topic, "co2");
-        assert.equal(value.data.co2, 779);
-
-        validateSchema(value.data, co2Schema, { throwError: true });
+        assert.equal(value.topic, "lifecycle");
+        assert.isObject(value.data);
+        assert.equal(value.data.batteryVoltage, 3.54);
+        assert.equal(value.data.batteryLevel, 85);
+        validateSchema(value.data, lifecycleSchema, { throwError: true });
       });
 
       consume(data);
+    });
+
+    it("should decode payload 3 with higher temperature and CO2", () => {
+      const data = {
+        data: {
+          port: 15,
+          payloadHex:
+            "040111094103022702030f32400314330008180000000000000003096101",
+        },
+      };
+
+      expectEmits((type, value) => {
+        assert.equal(type, "sample");
+        assert.equal(value.topic, "default");
+        assert.isObject(value.data);
+        assert.equal(value.data.temperature, 23.21);
+        assert.equal(value.data.humidity, 32.5);
+        assert.equal(value.data.co2, 551);
+        assert.equal(value.data.iaqIndex, 50);
+        assert.equal(value.data.iaqAccuracy, 1);
+        assert.equal(value.data.light, 51);
+        assert.notProperty(value.data, "pm2_5");
+        validateSchema(value.data, defaultSchema, { throwError: true });
+      });
+
+      expectEmits((type, value) => {
+        assert.equal(type, "sample");
+        assert.equal(value.topic, "lifecycle");
+        assert.isObject(value.data);
+        assert.equal(value.data.batteryVoltage, 3.53);
+        assert.equal(value.data.batteryLevel, 82);
+        validateSchema(value.data, lifecycleSchema, { throwError: true });
+      });
+
+      consume(data);
+    });
+
+    it("should decode payload 4 with highest CO2 reading", () => {
+      const data = {
+        data: {
+          port: 15,
+          payloadHex:
+            "04010b094103025e02030f32400314340008180000000000000003096001",
+        },
+      };
+
+      expectEmits((type, value) => {
+        assert.equal(type, "sample");
+        assert.equal(value.topic, "default");
+        assert.isObject(value.data);
+        assert.equal(value.data.temperature, 23.15);
+        assert.equal(value.data.humidity, 32.5);
+        assert.equal(value.data.co2, 606);
+        assert.equal(value.data.iaqIndex, 50);
+        assert.equal(value.data.iaqAccuracy, 1);
+        assert.equal(value.data.light, 52);
+        assert.notProperty(value.data, "pm2_5");
+        validateSchema(value.data, defaultSchema, { throwError: true });
+      });
+
+      expectEmits((type, value) => {
+        assert.equal(type, "sample");
+        assert.equal(value.topic, "lifecycle");
+        assert.isObject(value.data);
+        assert.equal(value.data.batteryVoltage, 3.52);
+        assert.equal(value.data.batteryLevel, 80);
+        validateSchema(value.data, lifecycleSchema, { throwError: true });
+      });
+
+      consume(data);
+    });
+
+    it("should decode payload 5 with different IAQ and humidity", () => {
+      const data = {
+        data: {
+          port: 15,
+          payloadHex:
+            "04010a094003020f02030f46400314410008180000000000000003095e01",
+        },
+      };
+
+      expectEmits((type, value) => {
+        assert.equal(type, "sample");
+        assert.equal(value.topic, "default");
+        assert.isObject(value.data);
+        assert.equal(value.data.temperature, 23.14);
+        assert.equal(value.data.humidity, 32);
+        assert.equal(value.data.co2, 527);
+        assert.equal(value.data.iaqIndex, 70);
+        assert.equal(value.data.iaqAccuracy, 1);
+        assert.equal(value.data.light, 65);
+        assert.notProperty(value.data, "pm2_5");
+        validateSchema(value.data, defaultSchema, { throwError: true });
+      });
+
+      expectEmits((type, value) => {
+        assert.equal(type, "sample");
+        assert.equal(value.topic, "lifecycle");
+        assert.isObject(value.data);
+        assert.equal(value.data.batteryVoltage, 3.5);
+        assert.equal(value.data.batteryLevel, 75);
+        validateSchema(value.data, lifecycleSchema, { throwError: true });
+      });
+
+      consume(data);
+    });
+
+    it("should decode payload 6 with non-zero PM values", () => {
+      const data = {
+        data: {
+          port: 15,
+          payloadHex:
+            "0401f208410302da01030f32400314330008187d005300d2000003096201",
+        },
+      };
+
+      expectEmits((type, value) => {
+        assert.equal(type, "sample");
+        assert.equal(value.topic, "default");
+        assert.isObject(value.data);
+        assert.equal(value.data.temperature, 22.9);
+        assert.equal(value.data.humidity, 32.5);
+        assert.equal(value.data.co2, 474);
+        assert.equal(value.data.iaqIndex, 50);
+        assert.equal(value.data.iaqAccuracy, 1);
+        assert.equal(value.data.light, 51);
+        assert.equal(value.data.pm2_5, 12.5);
+        assert.equal(value.data.pm1, 8.3);
+        assert.equal(value.data.pm10, 21);
+        assert.equal(value.data.pmObstructed, false);
+        validateSchema(value.data, defaultSchema, { throwError: true });
+      });
+
+      expectEmits((type, value) => {
+        assert.equal(type, "sample");
+        assert.equal(value.topic, "lifecycle");
+        assert.isObject(value.data);
+        assert.equal(value.data.batteryVoltage, 3.54);
+        assert.equal(value.data.batteryLevel, 85);
+        validateSchema(value.data, lifecycleSchema, { throwError: true });
+      });
+
+      consume(data);
+    });
+
+    it("should ignore payloads not on port 15", () => {
+      const data = {
+        data: {
+          port: 3,
+          payloadHex:
+            "0401f208410302da01030f32400314330008180000000000000003096201",
+        },
+      };
+
+      consume(data);
+      // No emits expected
     });
   });
 });
