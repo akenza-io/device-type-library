307de9934ed4ef2daef4b2ec882253e7b7ec62a0
J2 <jonasg@akenza.com>
2026-08-04 15:17:13 +0200

    Catching empty browan payloads

--- types/browan/TBDW100/uplink.js
+++ types/browan/TBDW100/uplink.js
@@ -56,33 +56,35 @@ function consume(event) {
   const data = {};
   const lifecycle = {};
 
-  data.open = !!Bits.bitsToUnsigned(bits.substr(7, 1));
+  if (payload.length !== 0) {
+    data.open = !!Bits.bitsToUnsigned(bits.substr(7, 1));
 
-  let batteryVoltage = Bits.bitsToUnsigned(bits.substr(12, 4));
-  batteryVoltage = (25 + batteryVoltage) / 10;
-  lifecycle.batteryVoltage = Math.round(batteryVoltage * 10) / 10;
+    let batteryVoltage = Bits.bitsToUnsigned(bits.substr(12, 4));
+    batteryVoltage = (25 + batteryVoltage) / 10;
+    lifecycle.batteryVoltage = Math.round(batteryVoltage * 10) / 10;
 
-  let batteryLevel =
-    Math.round((lifecycle.batteryVoltage - 3.1) / 0.005 / 10) * 10; // 3.1V - 3.6V
-  if (batteryLevel > 100) {
-    batteryLevel = 100;
-  } else if (batteryLevel < 0) {
-    batteryLevel = 0;
+    let batteryLevel =
+      Math.round((lifecycle.batteryVoltage - 3.1) / 0.005 / 10) * 10; // 3.1V - 3.6V
+    if (batteryLevel > 100) {
+      batteryLevel = 100;
+    } else if (batteryLevel < 0) {
+      batteryLevel = 0;
+    }
+    lifecycle.batteryLevel = batteryLevel;
+
+    data.temperature = Bits.bitsToUnsigned(bits.substr(17, 7));
+    data.temperature -= 32;
+    data.time = Hex.hexLittleEndianToBigEndian(payload.substr(6, 4), false);
+    data.count = Hex.hexLittleEndianToBigEndian(payload.substr(10, 6), false);
+
+    const state = event.state || {};
+    const calculated = calculateIncrement(state, data.count, checkForCustomFields(event.device, "usageCountDivider", 4), 2);
+    const { doorClosings, usageCount } = calculated.data;
+    data.relativeCount = calculated.data.increment;
+
+    emit("state", calculated.state);
+    emit("sample", { data: { doorClosings, usageCount }, topic: "door_count" });
+    emit("sample", { data: lifecycle, topic: "lifecycle" });
+    emit("sample", { data, topic: "default" });
   }
-  lifecycle.batteryLevel = batteryLevel;
-
-  data.temperature = Bits.bitsToUnsigned(bits.substr(17, 7));
-  data.temperature -= 32;
-  data.time = Hex.hexLittleEndianToBigEndian(payload.substr(6, 4), false);
-  data.count = Hex.hexLittleEndianToBigEndian(payload.substr(10, 6), false);
-
-  const state = event.state || {};
-  const calculated = calculateIncrement(state, data.count, checkForCustomFields(event.device, "usageCountDivider", 4), 2);
-  const { doorClosings, usageCount } = calculated.data;
-  data.relativeCount = calculated.data.increment;
-
-  emit("state", calculated.state);
-  emit("sample", { data: { doorClosings, usageCount }, topic: "door_count" });
-  emit("sample", { data: lifecycle, topic: "lifecycle" });
-  emit("sample", { data, topic: "default" });
 }
